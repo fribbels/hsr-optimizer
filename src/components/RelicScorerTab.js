f07ed517afa641258004adce5d43535006f6d8bd
Fribbels <fribbelsgithub@gmail.com>
2024-01-06 04:25:53 -0800

    Fix relic scorer url

--- src/components/RelicScorerTab.js
+++ src/components/RelicScorerTab.js
@@ -61,8 +61,8 @@ export default function RelicScorerTab(props) {
     setScorerId(x.scorerId);
     SaveState.save()
 
-    fetch('http://127.0.0.1:5000/getAccount', options)
-    // fetch('https://08hm0krwt2.execute-api.us-west-2.amazonaws.com/dev/getAccount', options)
+    // fetch('http://127.0.0.1:5000/getAccount', options)
+    fetch('https://08hm0krwt2.execute-api.us-west-2.amazonaws.com/dev/getAccount', options)
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);

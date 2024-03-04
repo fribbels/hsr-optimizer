7d685bea4114c737906e7793ae78e6d3518b65af
Fribbels <fribbelsgithub@gmail.com>
2024-03-03 21:51:13 -0800

    fix: realized one of our tests was using an incorrect light cone

---
+++ tests/OptimizerForm/lightConeConditionals/brighter-than-the-sun.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@playwright/test'
+
+test('test', async ({ page }) => {
+  await page.goto('/')
+  await page.getByText('Optimizer', { exact: true }).click()
+
+  await page.getByTitle('I Shall Be My Own Sword').click()
+  await page.getByText('Brighter Than the Sun').click()
+
+  await page.getByText('Dragon\'s Call stacks').hover()
+  const t1 = await page.locator('.ant-popover-content').getByText(`When the wearer uses their Basic ATK`)
+  expect(t1).toBeTruthy()
+})

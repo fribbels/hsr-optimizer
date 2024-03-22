7797740e158cd8eed9cba52ca6aaabad18e5659d
Fribbels <fribbelsgithub@gmail.com>
2024-03-22 13:08:41 -0700

    feat: remove tooltip for now, cleanup

--- src/components/relicPreview/GenerateStat.tsx
+++ src/components/relicPreview/GenerateStat.tsx
@@ -1,11 +1,10 @@
-import { Flex, Tooltip } from 'antd'
+import { Flex } from 'antd'
 import { Renderer } from 'lib/renderer'
 import { Assets } from 'lib/assets'
 import { Utils } from 'lib/utils'
 import { Constants, SubStats } from 'lib/constants'
 import { iconSize } from 'lib/constantsUi'
 import RelicStatText from 'components/relicPreview/RelicStatText'
-import { RelicRollGrader } from 'lib/relicRollGrader'
 import { Relic, StatRolls } from 'types/Relic'
 import { ReactElement } from 'react'
 import { RightIcon } from 'icons/RightIcon.jsx'
@@ -51,15 +50,15 @@ export const GenerateStat = (stat: Substat, main: boolean, relic: Relic) => {
       </Flex>
       {!main
         ? (
+          // <Tooltip title={`Roll quality: ${RelicRollGrader.calculateStatSum(stat.rolls)}%`}>
           <Flex justify="space-between" style={{ width: '41%' }}>
-            <Tooltip title={`Roll Quality: ${RelicRollGrader.calculateStatSum(stat.rolls)}%`}>
-              <Flex gap={0} align="center">
-                {stat.rolls
-                && generateRolls(stat)}
-              </Flex>
-            </Tooltip>
+            <Flex gap={0} align="center">
+              {stat.rolls
+              && generateRolls(stat)}
+            </Flex>
             <RelicStatText>{displayValue}</RelicStatText>
           </Flex>
+          // </Tooltip>
         )
         : (
           <RelicStatText>{displayValue}</RelicStatText>
@@ -72,10 +71,10 @@ function generateRolls(stat) {
   const rolls = Math.min(5, Math.max(0, stat.rolls.high + stat.rolls.mid + stat.rolls.low - 1))
   const result: ReactElement[] = []
   for (let i = 0; i < rolls; i++) {
-    result.push(<RightIcon key={i} style={{ fontSize: 10, marginRight: -7 }} />)
+    result.push(<RightIcon key={i} style={{ fontSize: 10, marginRight: -5 }} />)
   }
   if (rolls == 0) {
-    result.push(<div></div>)
+    result.push(<div key={0}></div>)
   }
   return (
     result
--- src/icons/RightIcon.jsx
+++ src/icons/RightIcon.jsx
@@ -1,9 +1,9 @@
 import Icon from '@ant-design/icons'
 
 const IconSVG = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
+  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24">
     <g transform="translate(24 0) scale(-1 1)">
-      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="2.5" d="M8 12L15 5M8 12L15 19" />
+      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="3.5" d="M8 12L15 5M8 12L15 19" />
     </g>
   </svg>
 )

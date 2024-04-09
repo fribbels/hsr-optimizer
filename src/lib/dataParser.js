358ce753228ad1932c3011e90577059dcf47474c
Fribbels <fribbelsgithub@gmail.com>
2024-04-09 10:57:24 -0700

    feat: add atk% chest for boothill

--- src/lib/dataParser.js
+++ src/lib/dataParser.js
@@ -3665,6 +3665,7 @@ function getScoringMetadata() {
         [Constants.Parts.Body]: [
           Constants.Stats.CR,
           Constants.Stats.CD,
+          Constants.Stats.ATK_P,
         ],
         [Constants.Parts.Feet]: [
           Constants.Stats.ATK_P,

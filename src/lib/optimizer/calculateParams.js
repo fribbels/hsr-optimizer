75a261403543862330492eecd49b09d49ef7640e
Fribbels <fribbelsgithub@gmail.com>
2024-05-05 17:38:54 -0700

    feat: add set conditionals

--- src/lib/optimizer/calculateParams.js
+++ src/lib/optimizer/calculateParams.js
@@ -36,6 +36,7 @@ function generateSetConditionalParams(request, params) {
   params.enabledCelestialDifferentiator = setConditionals[Constants.Sets.CelestialDifferentiator][1] == true ? 1 : 0
   params.enabledWatchmakerMasterOfDreamMachinations = setConditionals[Constants.Sets.WatchmakerMasterOfDreamMachinations][1] == true ? 1 : 0
   params.enabledIzumoGenseiAndTakamaDivineRealm = setConditionals[Constants.Sets.IzumoGenseiAndTakamaDivineRealm][1] == true ? 1 : 0
+  params.enabledForgeOfTheKalpagniLantern = setConditionals[Constants.Sets.ForgeOfTheKalpagniLantern][1] == true ? 1 : 0
 
   params.valueChampionOfStreetwiseBoxing = setConditionals[Constants.Sets.ChampionOfStreetwiseBoxing][1] || 0
   params.valueWastelanderOfBanditryDesert = setConditionals[Constants.Sets.WastelanderOfBanditryDesert][1] || 0
@@ -44,6 +45,8 @@ function generateSetConditionalParams(request, params) {
   params.valuePrisonerInDeepConfinement = setConditionals[Constants.Sets.PrisonerInDeepConfinement][1] || 0
   params.valuePioneerDiverOfDeadWaters = setConditionals[Constants.Sets.PioneerDiverOfDeadWaters][1] || 0
   params.valueSigoniaTheUnclaimedDesolation = setConditionals[Constants.Sets.SigoniaTheUnclaimedDesolation][1] || 0
+  params.valueTheWindSoaringValorous = setConditionals[Constants.Sets.TheWindSoaringValorous][1] || 0
+  params.valueDuranDynastyOfRunningWolves = setConditionals[Constants.Sets.DuranDynastyOfRunningWolves][1] || 0
 }
 
 function generateMultiplierParams(request, params) {

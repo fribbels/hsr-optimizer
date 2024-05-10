7a65d43af9f662e5e3d14e542b8a2d91a238e281
Fribbels <fribbelsgithub@gmail.com>
2024-05-09 23:51:10 -0700

    hotfix: changing reliquary to off for now (#339)

--- src/components/importerTab/ReliquaryDescription.tsx
+++ src/components/importerTab/ReliquaryDescription.tsx
@@ -5,10 +5,10 @@ import { ColorizedLink } from '../common/ColorizedLink'
 export function ReliquaryDescription(): ReactElement {
   return (
     <>
-      {(
+      {false && (
         <li>
           {'(Recommended) IceDynamix Reliquary Archiver'} (
-          <ColorizedLink text="Github" url={ReliquaryArchiverConfig.releases} />
+          <ColorizedLink text="Github" url={ReliquaryArchiverConfig.releases}/>
           )
           <ul>
             <li>Network scanner</li>

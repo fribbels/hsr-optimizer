31a8bf3143dc088c17aa7aff7ac46f2adedacd39
IceDynamix <me@icedynamix.moe>
2024-05-11 00:06:58 +0200

    fix: bump reliquary archiver version to v0.1.6 (#342)

--- src/lib/importer/importConfig.js
+++ src/lib/importer/importConfig.js
@@ -19,7 +19,7 @@ export const ReliquaryArchiverConfig = {
   releases: 'https://github.com/IceDynamix/reliquary-archiver/releases/latest',
   defaultFileName: 'archiver_output.json',
   sourceString: 'reliquary_archiver',
-  latestBuildVersion: 'v0.1.5',
+  latestBuildVersion: 'v0.1.6',
   latestOutputVersion: 3,
   speedVerified: true,
 }

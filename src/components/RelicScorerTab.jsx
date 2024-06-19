8c84ad1f48cf1b199b2834f66de7a54636613080
Fribbels <fribbelsgithub@gmail.com>
2024-06-19 00:26:21 -0400

    fix: relic scorer banner

--- src/components/RelicScorerTab.jsx
+++ src/components/RelicScorerTab.jsx
@@ -140,9 +140,9 @@ export default function RelicScorerTab() {
   return (
     <div>
       <Flex vertical gap={0} align="center">
-        {/* <Flex gap={10} vertical align='center'> */}
-        {/* <Text><h2>The relic scorer is down for maintenance after the 2.2 patch - stay tuned!</h2></Text> */}
-        {/* </Flex> */}
+        <Flex gap={10} vertical align="center">
+          <Text><h2>The relic scorer is down for maintenance after the 2.2 patch - stay tuned!</h2></Text>
+        </Flex>
         <Flex gap={10} vertical align="center">
           <Text>Enter your account UID to score your profile characters at level 80 with maxed traces. Log out of the game to refresh instantly.</Text>
         </Flex>

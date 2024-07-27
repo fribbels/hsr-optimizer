eea4cff64c2686ce1bfc4f4d8501e9b1264f7afa
Fribbels <fribbelsgithub@gmail.com>
2024-07-27 13:41:44 -0700

    refactor: updating button styling

--- src/components/CharacterModal.jsx
+++ src/components/CharacterModal.jsx
@@ -1,59 +1,19 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import { Button, Flex, Form, Modal, Radio } from 'antd'
 import { HeaderText } from './HeaderText'
-import { defaultGap } from 'lib/constantsUi'
 import PropTypes from 'prop-types'
 import LightConeSelect from 'components/optimizerTab/optimizerForm/LightConeSelect'
 import CharacterSelect from 'components/optimizerTab/optimizerForm/CharacterSelect'
 import DB from 'lib/db'
-
-// Keep new characters/lcs at the top of the list for convenience. More popular should be at the bottom
-// TODO: These no longer work because we changed the character selector
-// const pinnedValues = [
-//   'Black Swan',
-//   'Sparkle',
-//   'Gallagher',
-//   'Aventurine',
-//   'Acheron',
-//
-//   'Reforged Remembrance',
-//   'Earthly Escapade',
-//   'Concert for Two',
-//   'Inherently Unjust Destiny',
-//   'Along the Passing Shore',
-// ]
-// function generatePinnedList(list) {
-//   if (!list || !list.length) return []
-//
-//   list.sort((a, b) => {
-//     const indexA = pinnedValues.indexOf(a.label)
-//     const indexB = pinnedValues.indexOf(b.label)
-//     if (indexB > indexA) {
-//       return 1
-//     } else if (indexA > indexB) {
-//       return -1
-//     }
-//
-//     return a.label.localeCompare(b.label)
-//   })
-//
-//   list.map((option) => pinnedValues.indexOf(option.label) > -1 ? option.label = '(New!) ' + option.label : null)
-//
-//   return list
-// }
 
 export default function CharacterModal(props) {
   const [characterForm] = Form.useForm()
   window.characterForm = characterForm
 
   const [characterId, setCharacterId] = useState('')
-
   const [eidolon, setEidolon] = useState(props.initialCharacter?.form.characterEidolon || 0)
-
   const [superimposition, setSuperimposition] = useState(props.initialCharacter?.form.lightConeSuperimposition || 1)
-
   const characterMetadata = useMemo(() => DB.getMetadata().characters, [])
-
   const initialPath = !props.addCharacter && props.initialCharacter ? characterMetadata[props.initialCharacter.form.characterId].path : undefined
 
   useEffect(() => {
@@ -106,52 +66,55 @@ export default function CharacterModal(props) {
         preserve={false}
         layout="vertical"
       >
-        <Flex justify="space-between" align="center">
-          <HeaderText>Character</HeaderText>
-        </Flex>
+        <Flex vertical gap={10}>
+          <Flex vertical gap={5}>
+            <HeaderText>Character</HeaderText>
+            <Form.Item size="default" name="characterId">
+              <CharacterSelect
+                value=""
+                onChange={setCharacterId}
+                withIcon={true}
+              />
+            </Form.Item>
+            <Form.Item size="default" name="characterEidolon">
+              <Radio.Group
+                value={eidolon}
+                onChange={(e) => setEidolon(e.target.value)}
+                buttonStyle='solid'
+                style={{width: '100%', display: 'flex'}}
+              >
+                <RadioButton text='E0' value={0}/>
+                <RadioButton text='E1' value={1}/>
+                <RadioButton text='E2' value={2}/>
+                <RadioButton text='E3' value={3}/>
+                <RadioButton text='E4' value={4}/>
+                <RadioButton text='E5' value={5}/>
+                <RadioButton text='E6' value={6}/>
+              </Radio.Group>
+            </Form.Item>
+          </Flex>
 
-        <Flex vertical gap={defaultGap} style={{ marginBottom: 10 }}>
-          <Form.Item size="default" name="characterId">
-            <CharacterSelect
-              value=""
-              selectStyle={{ width: panelWidth - 22 - defaultGap }}
-              onChange={setCharacterId}
-              withIcon={true}
-            />
-          </Form.Item>
-          <Form.Item size="default" name="characterEidolon">
-            <Radio.Group value={eidolon} onChange={(e) => setEidolon(e.target.value)}>
-              <Radio.Button value={0}>E0</Radio.Button>
-              <Radio.Button value={1}>E1</Radio.Button>
-              <Radio.Button value={2}>E2</Radio.Button>
-              <Radio.Button value={3}>E3</Radio.Button>
-              <Radio.Button value={4}>E4</Radio.Button>
-              <Radio.Button value={5}>E5</Radio.Button>
-              <Radio.Button value={6}>E6</Radio.Button>
-            </Radio.Group>
-          </Form.Item>
-        </Flex>
-
-        <Flex justify="space-between" align="center">
-          <HeaderText>Light cone</HeaderText>
-        </Flex>
-        <Flex vertical gap={defaultGap}>
-          <Form.Item size="default" name="lightCone">
-            <LightConeSelect
-              value=""
-              selectStyle={{ width: panelWidth - 22 - defaultGap }}
-              characterId={characterId}
-              initialPath={initialPath}
-            />
-          </Form.Item>
-          <Flex>
+          <Flex vertical gap={5}>
+            <HeaderText>Light cone</HeaderText>
+            <Form.Item size="default" name="lightCone">
+              <LightConeSelect
+                value=""
+                characterId={characterId}
+                initialPath={initialPath}
+              />
+            </Form.Item>
             <Form.Item size="default" name="lightConeSuperimposition">
-              <Radio.Group value={superimposition} onChange={(e) => setSuperimposition(e.target.value)}>
-                <Radio.Button value={1}>S1</Radio.Button>
-                <Radio.Button value={2}>S2</Radio.Button>
-                <Radio.Button value={3}>S3</Radio.Button>
-                <Radio.Button value={4}>S4</Radio.Button>
-                <Radio.Button value={5}>S5</Radio.Button>
+              <Radio.Group
+                value={superimposition}
+                onChange={(e) => setSuperimposition(e.target.value)}
+                buttonStyle='solid'
+                style={{width: '100%', display: 'flex'}}
+              >
+                <RadioButton text='S1' value={1}/>
+                <RadioButton text='S2' value={2}/>
+                <RadioButton text='S3' value={3}/>
+                <RadioButton text='S4' value={4}/>
+                <RadioButton text='S5' value={5}/>
               </Radio.Group>
             </Form.Item>
           </Flex>
@@ -166,4 +129,11 @@ CharacterModal.propTypes = {
   setOpen: PropTypes.func,
   initialCharacter: PropTypes.object,
   addCharacter: PropTypes.bool,
+}
+
+// Full width radio buttons
+function RadioButton(props) {
+  return (
+    <Radio.Button value={props.value} style={{flex: 1, textAlign: 'center'}}>{props.text}</Radio.Button>
+  )
 }

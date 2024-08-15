435a08f1d5973502c257f3af7d2f48a1ebab63eb
Fribbels <fribbelsgithub@gmail.com>
2024-08-15 01:04:42 -0700

    feat: rename weight filter

--- src/components/optimizerTab/optimizerForm/SubstatWeightFilters.tsx
+++ src/components/optimizerTab/optimizerForm/SubstatWeightFilters.tsx
@@ -14,26 +14,26 @@ export const SubstatWeightFilters = () => {
       <Flex vertical gap={0}>
         <Flex justify="space-between" align="center">
           <HeaderText>Substat weight filter</HeaderText>
-          <TooltipImage type={Hint.substatWeightFilter()} />
+          <TooltipImage type={Hint.substatWeightFilter()}/>
         </Flex>
 
-        <FormStatRollSlider text="HP" name={Constants.Stats.HP_P} />
-        <FormStatRollSlider text="ATK" name={Constants.Stats.ATK_P} />
-        <FormStatRollSlider text="DEF" name={Constants.Stats.DEF_P} />
-        <FormStatRollSlider text="SPD" name={Constants.Stats.SPD} />
-        <FormStatRollSlider text="CR" name={Constants.Stats.CR} />
-        <FormStatRollSlider text="CD" name={Constants.Stats.CD} />
-        <FormStatRollSlider text="EHR" name={Constants.Stats.EHR} />
-        <FormStatRollSlider text="RES" name={Constants.Stats.RES} />
-        <FormStatRollSlider text="BE" name={Constants.Stats.BE} />
+        <FormStatRollSlider text="HP" name={Constants.Stats.HP_P}/>
+        <FormStatRollSlider text="ATK" name={Constants.Stats.ATK_P}/>
+        <FormStatRollSlider text="DEF" name={Constants.Stats.DEF_P}/>
+        <FormStatRollSlider text="SPD" name={Constants.Stats.SPD}/>
+        <FormStatRollSlider text="CR" name={Constants.Stats.CR}/>
+        <FormStatRollSlider text="CD" name={Constants.Stats.CD}/>
+        <FormStatRollSlider text="EHR" name={Constants.Stats.EHR}/>
+        <FormStatRollSlider text="RES" name={Constants.Stats.RES}/>
+        <FormStatRollSlider text="BE" name={Constants.Stats.BE}/>
       </Flex>
 
       <Flex vertical gap={3}>
-        <HeaderText>Min weighted rolls per relic</HeaderText>
+        <HeaderText>Weighted rolls per relic</HeaderText>
         <Flex vertical gap={5}>
-          <FormStatRollSliderTopPercent index={0} />
-          <FormStatRollSliderTopPercent index={1} />
-          <FormStatRollSliderTopPercent index={2} />
+          <FormStatRollSliderTopPercent index={0}/>
+          <FormStatRollSliderTopPercent index={1}/>
+          <FormStatRollSliderTopPercent index={2}/>
         </Flex>
       </Flex>
     </Flex>

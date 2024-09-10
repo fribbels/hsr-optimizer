60e80aa6678e923178b5f9cc1ca84b84becd92ef
Fribbels <fribbelsgithub@gmail.com>
2024-09-09 17:37:53 -0700

    hotfix: empty old scoring metadata bug

--- src/lib/db.js
+++ src/lib/db.js
@@ -460,7 +460,7 @@ export const DB = {
         // After this migration done, Ctrl + F and uncomment the POST MIGRATION UNCOMMENT section to re-enable overwriting
         const scoringMetadataOverrides = x.scoringMetadataOverrides[key]
         if (scoringMetadataOverrides) {
-          const oldScoringMetadataStats = oldCharacterScoringMetadata[key]
+          const oldScoringMetadataStats = oldCharacterScoringMetadata[key] || {}
           const defaultScoringMetadata = dbCharacters[key].scoringMetadata
 
           let isOldScoring = true

8ad72c99da72342b15a31da26a13b552fda47d41
Fribbels <fribbelsgithub@gmail.com>
2024-09-26 01:50:52 -0700

    feat: hide rating filters into collapse (#587)
    * feat: hide unused filter options
    
    * fix: styling

--- src/components/optimizerTab/optimizerForm/ResultFilters.tsx
+++ src/components/optimizerTab/optimizerForm/ResultFilters.tsx
@@ -4,47 +4,65 @@ import { TooltipImage } from 'components/TooltipImage.jsx'
 import { Hint } from 'lib/hint.jsx'
 import FilterRow from 'components/optimizerTab/optimizerForm/FilterRow.tsx'
 import { optimizerTabDefaultGap } from 'components/optimizerTab/optimizerTabConstants.ts'
+import { CaretDownOutlined } from '@ant-design/icons'
+import { useState } from 'react'
 
 export const MinMaxStatFilters = () => {
-  return (
-    <Flex vertical gap={optimizerTabDefaultGap}>
-      <Flex justify='space-between' align='center'>
-        <HeaderText>Stat min / max filters</HeaderText>
-        <TooltipImage type={Hint.statFilters()}/>
-      </Flex>
-      <Flex vertical gap={5}>
-        <FilterRow name='Hp' label='HP'/>
-        <FilterRow name='Atk' label='ATK'/>
-        <FilterRow name='Def' label='DEF'/>
-        <FilterRow name='Spd' label='SPD'/>
-        <FilterRow name='Cr' label='CR'/>
-        <FilterRow name='Cd' label='CD'/>
-        <FilterRow name='Ehr' label='EHR'/>
-        <FilterRow name='Res' label='RES'/>
-        <FilterRow name='Be' label='BE'/>
-        <FilterRow name='Err' label='ERR'/>
-      </Flex>
-    </Flex>
-  )
-}
+  const [view, setView] = useState('Stats')
 
-export const MinMaxRatingFilters = () => {
   return (
-    <Flex vertical gap={optimizerTabDefaultGap}>
-      <Flex justify='space-between' align='center'>
-        <HeaderText>Rating min / max filters</HeaderText>
-        <TooltipImage type={Hint.ratingFilters()}/>
+    <div>
+      <Flex vertical gap={optimizerTabDefaultGap} style={{ display: view == 'Stats' ? 'flex' : 'none' }}>
+        <Flex justify='space-between' align='center'>
+          <HeaderText>Stat min / max filters</HeaderText>
+          <TooltipImage type={Hint.statFilters()}/>
+        </Flex>
+        <Flex vertical gap={5}>
+          <FilterRow name='Hp' label='HP'/>
+          <FilterRow name='Atk' label='ATK'/>
+          <FilterRow name='Def' label='DEF'/>
+          <FilterRow name='Spd' label='SPD'/>
+          <FilterRow name='Cr' label='CR'/>
+          <FilterRow name='Cd' label='CD'/>
+          <FilterRow name='Ehr' label='EHR'/>
+          <FilterRow name='Res' label='RES'/>
+          <FilterRow name='Be' label='BE'/>
+          <FilterRow name='Err' label='ERR'/>
+        </Flex>
+        <Flex
+          style={{ height: 20, width: '100%', marginTop: -4, cursor: 'pointer' }}
+          justify='space-around'
+          align='flex-start'
+          onClick={() => setView('Ratings')}
+        >
+          <CaretDownOutlined/>
+        </Flex>
       </Flex>
 
-      <FilterRow name='Weight' label='WEIGHT'/>
-      <FilterRow name='Ehp' label='EHP'/>
-      <FilterRow name='Basic' label='BASIC'/>
-      <FilterRow name='Skill' label='SKILL'/>
-      <FilterRow name='Ult' label='ULT'/>
-      <FilterRow name='Fua' label='FUA'/>
-      <FilterRow name='Dot' label='DOT'/>
-      <FilterRow name='Break' label='BREAK'/>
-      <FilterRow name='Combo' label='COMBO'/>
-    </Flex>
+      <Flex vertical gap={optimizerTabDefaultGap} style={{ display: view == 'Ratings' ? 'flex' : 'none' }}>
+        <Flex justify='space-between' align='center'>
+          <HeaderText>Rating min / max filters</HeaderText>
+          <TooltipImage type={Hint.ratingFilters()}/>
+        </Flex>
+
+        <FilterRow name='Weight' label='WEIGHT'/>
+        <FilterRow name='Ehp' label='EHP'/>
+        <FilterRow name='Basic' label='BASIC'/>
+        <FilterRow name='Skill' label='SKILL'/>
+        <FilterRow name='Ult' label='ULT'/>
+        <FilterRow name='Fua' label='FUA'/>
+        <FilterRow name='Dot' label='DOT'/>
+        <FilterRow name='Break' label='BREAK'/>
+        <FilterRow name='Combo' label='COMBO'/>
+        <Flex
+          style={{ height: 20, width: '100%', marginTop: -4, cursor: 'pointer' }}
+          justify='space-around'
+          align='flex-start'
+          onClick={() => setView('Stats')}
+        >
+          <CaretDownOutlined/>
+        </Flex>
+      </Flex>
+    </div>
   )
 }

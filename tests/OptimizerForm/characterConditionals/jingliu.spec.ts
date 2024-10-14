6c3decfa5d791de79d4166721c7647ca954c0784
Fribbels <fribbelsgithub@gmail.com>
2024-10-14 02:12:02 -0700

    fix: playwright tests

--- tests/OptimizerForm/characterConditionals/jingliu.spec.ts
+++ tests/OptimizerForm/characterConditionals/jingliu.spec.ts
@@ -4,11 +4,11 @@ test('test', async ({ page }) => {
   await page.goto('/#scorer')
   await page.getByText('Optimizer', { exact: true }).click()
 
-  await page.getByText('Enhanced state').hover()
+  await page.locator('#OPTIMIZER').getByText('Enhanced state').hover()
   const t1 = await page.locator('.ant-popover-content').getByText('When Jingliu has 2 stack(s)')
   expect(t1).toBeTruthy()
 
-  await page.getByText('HP drain ATK buff').click()
+  await page.locator('#OPTIMIZER').getByText('HP drain ATK buff').click()
   const t2 = await page.locator('.ant-popover-content').getByText("Jingliu's ATK increases by 540%")
   expect(t2).toBeTruthy()
 })

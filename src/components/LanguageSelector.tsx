b4a1e264510b19b118b795e5310e13664bb439e4
FI00ds <lucasrobertson368+github@gmail.com>
2024-11-04 17:33:34 +0000

    add chinese and portuguese to ocmpleted languages

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,25 +1,28 @@
 import { Button, Flex, Select } from 'antd'
 import { useTranslation } from 'react-i18next'
 import { languages } from 'lib/i18n'
-import { ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import { Assets } from 'lib/assets'
 
 type placementOptions = 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topRight'
 
-const completedLocales = ['en']
+type Languages = keyof typeof languages
+
+const completedLocales: Languages[] = ['en', 'zh', 'pt'] as const
 
 export function LanguageSelector(props: { style: React.CSSProperties; dropdownStyle: React.CSSProperties; flagOnly: boolean; placement: placementOptions }) {
-  const { t, i18n } = useTranslation()
-  const selectOptions = ((languages: { [key: string]: { locale: string; nativeName: string } }) => {
+  const { i18n } = useTranslation()
+  const selectOptions = ((locales: { [key in Languages]: { locale: string; nativeName: string } }) => {
     const ret: { value: string; label: ReactElement }[] = []
-    for (const key in languages) {
+    for (const key in locales) {
       ret.push({
-        value: languages[key].locale,
+        value: locales[key as Languages].locale,
         label: (
           <Flex gap={8}>
-            <img style={{ height: 15, marginTop: 4 }} src={Assets.getFlag(languages[key].locale)}/>
-            {languages[key].nativeName}
-            {completedLocales.includes(languages[key].locale) ? '' : ' (WIP)'}
+            <img style={{ height: 15, marginTop: 4 }} src={Assets.getFlag(locales[key as Languages].locale)}/>
+            {locales[key as Languages].nativeName}
+            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access */}
+            {completedLocales.includes(locales[key].locale) ? '' : ' (WIP)'}
           </Flex>
         ),
       })
@@ -34,11 +37,11 @@ export function LanguageSelector(props: { style: React.CSSProperties; dropdownSt
         if (!props.flagOnly) {
           return (
             <Flex gap={8}>
-              <img style={{ height: 15, marginTop: 8 }} src={Assets.getFlag(value)}/>{languages[value].nativeName}
+              <img style={{ height: 15, marginTop: 8 }} src={Assets.getFlag(value as Languages)}/>{languages[value as Languages].nativeName}
             </Flex>
           )
         }
-        return <img style={{ height: 15, marginTop: 8 }} src={Assets.getFlag(value)}/>
+        return <img style={{ height: 15, marginTop: 8 }} src={Assets.getFlag(value as Languages)}/>
       }}
       onChange={(e: string) => {
         i18n.changeLanguage(e)

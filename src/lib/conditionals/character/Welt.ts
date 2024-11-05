1bd88d797c2891a87087a9922f7ba021140c5abe
Fribbels <fribbelsgithub@gmail.com>
2024-11-05 14:48:34 -0800

    feat: migrate welt

--- src/lib/conditionals/character/Welt.ts
+++ src/lib/conditionals/character/Welt.ts
@@ -1,5 +1,5 @@
-import { AbilityEidolon, Conditionals, ContentDefinition, findContentId, gpuStandardAtkFinalizer, standardAtkFinalizer } from 'lib/conditionals/conditionalUtils'
-import { ComputedStatsArray, Key } from 'lib/optimizer/computedStatsArray'
+import { AbilityEidolon, Conditionals, ContentDefinition, gpuStandardAtkFinalizer, standardAtkFinalizer } from 'lib/conditionals/conditionalUtils'
+import { ComputedStatsArray, Key, Source } from 'lib/optimizer/computedStatsArray'
 import { TsUtils } from 'lib/TsUtils'
 
 import { Eidolon } from 'types/Character'
@@ -17,20 +17,20 @@ export default (e: Eidolon, withContent: boolean): CharacterConditional => {
   const ultScaling = ult(e, 1.50, 1.62)
   const talentScaling = talent(e, 0.60, 0.66)
 
-  const content: ContentDefinition<typeof defaults> = [
-    {
+  const content: ContentDefinition<typeof defaults> = {
+    enemyDmgTakenDebuff: {
       formItem: 'switch',
       id: 'enemyDmgTakenDebuff',
       text: t('Content.enemyDmgTakenDebuff.text'),
       content: t('Content.enemyDmgTakenDebuff.content'),
     },
-    {
+    enemySlowed: {
       formItem: 'switch',
       id: 'enemySlowed',
       text: t('Content.enemySlowed.text'),
       content: t('Content.enemySlowed.content', { talentScaling: TsUtils.precisionRound(100 * talentScaling) }),
     },
-    {
+    skillExtraHits: {
       formItem: 'slider',
       id: 'skillExtraHits',
       text: t('Content.skillExtraHits.text'),
@@ -38,61 +38,65 @@ export default (e: Eidolon, withContent: boolean): CharacterConditional => {
       min: 0,
       max: skillExtraHitsMax,
     },
-    {
+    e1EnhancedState: {
       formItem: 'switch',
       id: 'e1EnhancedState',
       text: t('Content.e1EnhancedState.text'),
       content: t('Content.e1EnhancedState.content'),
       disabled: (e < 1),
     },
-  ]
+  }
 
-  const teammateContent: ContentDefinition<typeof teammateDefaults> = [
-    findContentId(content, 'enemyDmgTakenDebuff'),
-  ]
+  const teammateContent: ContentDefinition<typeof teammateDefaults> = {
+    enemyDmgTakenDebuff: content.enemyDmgTakenDebuff,
+  }
+
+  const defaults = {
+    enemySlowed: true,
+    enemyDmgTakenDebuff: true,
+    skillExtraHits: skillExtraHitsMax,
+    e1EnhancedState: true,
+  }
+
+  const teammateDefaults = {
+    enemyDmgTakenDebuff: true,
+  }
 
   return {
     content: () => Object.values(content),
     teammateContent: () => Object.values(teammateContent),
-    defaults: () => ({
-      enemySlowed: true,
-      enemyDmgTakenDebuff: true,
-      skillExtraHits: skillExtraHitsMax,
-      e1EnhancedState: true,
-    }),
-    teammateDefaults: () => ({
-      enemyDmgTakenDebuff: true,
-    }),
+    defaults: () => defaults,
+    teammateDefaults: () => teammateDefaults,
     precomputeEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r: Conditionals<typeof content> = action.characterConditionals
 
       // Stats
-      x.ELEMENTAL_DMG += (x.a[Key.ENEMY_WEAKNESS_BROKEN]) ? 0.20 : 0
+      x.ELEMENTAL_DMG.buff((x.a[Key.ENEMY_WEAKNESS_BROKEN]) ? 0.20 : 0, Source.NONE)
 
       // Scaling
-      x.BASIC_SCALING += basicScaling
-      x.SKILL_SCALING += skillScaling
-      x.ULT_SCALING += ultScaling
+      x.BASIC_SCALING.buff(basicScaling, Source.NONE)
+      x.SKILL_SCALING.buff(skillScaling, Source.NONE)
+      x.ULT_SCALING.buff(ultScaling, Source.NONE)
 
-      x.BASIC_SCALING += (r.enemySlowed) ? talentScaling : 0
-      x.SKILL_SCALING += (r.enemySlowed) ? talentScaling : 0
-      x.ULT_SCALING += (r.enemySlowed) ? talentScaling : 0
+      x.BASIC_SCALING.buff((r.enemySlowed) ? talentScaling : 0, Source.NONE)
+      x.SKILL_SCALING.buff((r.enemySlowed) ? talentScaling : 0, Source.NONE)
+      x.ULT_SCALING.buff((r.enemySlowed) ? talentScaling : 0, Source.NONE)
 
-      x.BASIC_SCALING += (e >= 1 && r.e1EnhancedState) ? 0.50 * basicScaling : 0
-      x.SKILL_SCALING += (e >= 1 && r.e1EnhancedState) ? 0.80 * skillScaling : 0
+      x.BASIC_SCALING.buff((e >= 1 && r.e1EnhancedState) ? 0.50 * basicScaling : 0, Source.NONE)
+      x.SKILL_SCALING.buff((e >= 1 && r.e1EnhancedState) ? 0.80 * skillScaling : 0, Source.NONE)
 
-      x.SKILL_SCALING += r.skillExtraHits * skillScaling
+      x.SKILL_SCALING.buff(r.skillExtraHits * skillScaling, Source.NONE)
 
-      x.BASIC_TOUGHNESS_DMG += 30
-      x.SKILL_TOUGHNESS_DMG += 30 + 30 * r.skillExtraHits
-      x.ULT_TOUGHNESS_DMG += 60
+      x.BASIC_TOUGHNESS_DMG.buff(30, Source.NONE)
+      x.SKILL_TOUGHNESS_DMG.buff(30 + 30 * r.skillExtraHits, Source.NONE)
+      x.ULT_TOUGHNESS_DMG.buff(60, Source.NONE)
 
       return x
     },
     precomputeMutualEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const m: Conditionals<typeof teammateContent> = action.characterConditionals
 
-      x.VULNERABILITY += (m.enemyDmgTakenDebuff) ? 0.12 : 0
+      x.VULNERABILITY.buff((m.enemyDmgTakenDebuff) ? 0.12 : 0, Source.NONE)
     },
     finalizeCalculations: (x: ComputedStatsArray) => standardAtkFinalizer(x),
     gpuFinalizeCalculations: () => gpuStandardAtkFinalizer(),
--- src/types/Conditionals.d.ts
+++ src/types/Conditionals.d.ts
@@ -60,11 +60,11 @@ export type ContentComponentMap = {
 // extracted content to apply to <DisplayFormControl />
 export type ContentItem = {
   [K in keyof ContentComponentMap]: {
-  formItem: K
-  id: string
-  content: string
-  teammateIndex?: number
-} & Omit<ComponentProps<ContentComponentMap[K]>, 'content' | 'title'>
+    formItem: K
+    id: string
+    content: string
+    teammateIndex?: number
+  } & Omit<ComponentProps<ContentComponentMap[K]>, 'content' | 'title'>
 }[keyof ContentComponentMap]
 
 export type ConditionalBuff =
@@ -447,3 +447,4 @@ export type ConditionalBuff =
   | 'shieldAbility'
   | 'be250Buff'
   | 'e4BreakDmg'
+  | 'skillAtkBoost'

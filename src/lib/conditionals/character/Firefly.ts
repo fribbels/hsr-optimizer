7a94f32d0ea5a4dc2d4571aa769cc24fe7daef76
Fribbels <fribbelsgithub@gmail.com>
2024-11-04 23:22:31 -0800

    feat: migrate firefly

--- src/lib/conditionals/character/Firefly.ts
+++ src/lib/conditionals/character/Firefly.ts
@@ -1,10 +1,9 @@
 import { BREAK_TYPE } from 'lib/conditionals/conditionalConstants'
 import { AbilityEidolon, Conditionals, ContentDefinition } from 'lib/conditionals/conditionalUtils'
-import { Stats } from 'lib/constants'
 import { FireflyConversionConditional } from 'lib/gpu/conditionals/dynamicConditionals'
 import { wgslTrue } from 'lib/gpu/injection/wgslUtils'
 import { buffAbilityVulnerability } from 'lib/optimizer/calculateBuffs'
-import { ComputedStatsArray, Key } from 'lib/optimizer/computedStatsArray'
+import { ComputedStatsArray, Key, Source } from 'lib/optimizer/computedStatsArray'
 import { TsUtils } from 'lib/TsUtils'
 
 import { Eidolon } from 'types/Character'
@@ -26,65 +25,6 @@ export default (e: Eidolon, withContent: boolean): CharacterConditional => {
   const talentResBuff = talent(e, 0.30, 0.34)
   const talentDmgReductionBuff = talent(e, 0.40, 0.44)
 
-  const content: ContentDefinition<typeof defaults> = [
-    {
-      formItem: 'switch',
-      id: 'enhancedStateActive',
-      text: t('Content.enhancedStateActive.text'),
-      content: t('Content.enhancedStateActive.content'),
-    },
-    {
-      formItem: 'switch',
-      id: 'enhancedStateSpdBuff',
-      text: t('Content.enhancedStateSpdBuff.text'),
-      content: t('Content.enhancedStateSpdBuff.content', { ultSpdBuff }),
-    },
-    {
-      formItem: 'switch',
-      id: 'superBreakDmg',
-      text: t('Content.superBreakDmg.text'),
-      content: t('Content.superBreakDmg.content'),
-    },
-    {
-      formItem: 'switch',
-      id: 'atkToBeConversion',
-      text: t('Content.atkToBeConversion.text'),
-      content: t('Content.atkToBeConversion.content'),
-    },
-    {
-      formItem: 'switch',
-      id: 'talentDmgReductionBuff',
-      text: t('Content.talentDmgReductionBuff.text'),
-      content: t('Content.talentDmgReductionBuff.content', {
-        talentResBuff: TsUtils.precisionRound(100 * talentResBuff),
-        talentDmgReductionBuff: TsUtils.precisionRound(100 * talentDmgReductionBuff),
-      }),
-    },
-    {
-      formItem: 'switch',
-      id: 'e1DefShred',
-      text: t('Content.e1DefShred.text'),
-      content: t('Content.e1DefShred.content'),
-      disabled: e < 1,
-    },
-    {
-      formItem: 'switch',
-      id: 'e4ResBuff',
-      text: t('Content.e4ResBuff.text'),
-      content: t('Content.e4ResBuff.content'),
-      disabled: e < 4,
-    },
-    {
-      formItem: 'switch',
-      id: 'e6Buffs',
-      text: t('Content.e6Buffs.text'),
-      content: t('Content.e6Buffs.content'),
-      disabled: e < 6,
-    },
-  ]
-
-  const teammateContent: ContentDefinition<typeof teammateDefaults> = []
-
   const defaults = {
     enhancedStateActive: true,
     enhancedStateSpdBuff: true,
@@ -96,53 +36,110 @@ export default (e: Eidolon, withContent: boolean): CharacterConditional => {
     e6Buffs: true,
   }
 
+  const content: ContentDefinition<typeof defaults> = {
+    enhancedStateActive: {
+      formItem: 'switch',
+      id: 'enhancedStateActive',
+      text: t('Content.enhancedStateActive.text'),
+      content: t('Content.enhancedStateActive.content'),
+    },
+    enhancedStateSpdBuff: {
+      formItem: 'switch',
+      id: 'enhancedStateSpdBuff',
+      text: t('Content.enhancedStateSpdBuff.text'),
+      content: t('Content.enhancedStateSpdBuff.content', { ultSpdBuff }),
+    },
+    superBreakDmg: {
+      formItem: 'switch',
+      id: 'superBreakDmg',
+      text: t('Content.superBreakDmg.text'),
+      content: t('Content.superBreakDmg.content'),
+    },
+    atkToBeConversion: {
+      formItem: 'switch',
+      id: 'atkToBeConversion',
+      text: t('Content.atkToBeConversion.text'),
+      content: t('Content.atkToBeConversion.content'),
+    },
+    talentDmgReductionBuff: {
+      formItem: 'switch',
+      id: 'talentDmgReductionBuff',
+      text: t('Content.talentDmgReductionBuff.text'),
+      content: t('Content.talentDmgReductionBuff.content', {
+        talentResBuff: TsUtils.precisionRound(100 * talentResBuff),
+        talentDmgReductionBuff: TsUtils.precisionRound(100 * talentDmgReductionBuff),
+      }),
+    },
+    e1DefShred: {
+      formItem: 'switch',
+      id: 'e1DefShred',
+      text: t('Content.e1DefShred.text'),
+      content: t('Content.e1DefShred.content'),
+      disabled: e < 1,
+    },
+    e4ResBuff: {
+      formItem: 'switch',
+      id: 'e4ResBuff',
+      text: t('Content.e4ResBuff.text'),
+      content: t('Content.e4ResBuff.content'),
+      disabled: e < 4,
+    },
+    e6Buffs: {
+      formItem: 'switch',
+      id: 'e6Buffs',
+      text: t('Content.e6Buffs.text'),
+      content: t('Content.e6Buffs.content'),
+      disabled: e < 6,
+    },
+  }
+
   return {
     content: () => Object.values(content),
-    teammateContent: () => Object.values(teammateContent),
     defaults: () => defaults,
-    teammateDefaults: () => ({}),
     initializeConfigurations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r: Conditionals<typeof content> = action.characterConditionals
 
       if (r.superBreakDmg) {
-        x.ENEMY_WEAKNESS_BROKEN = 1
+        x.ENEMY_WEAKNESS_BROKEN.set(1, Source.NONE)
       }
     },
     precomputeEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r: Conditionals<typeof content> = action.characterConditionals
 
-      x[Stats.RES] += (r.enhancedStateActive) ? talentResBuff : 0
-      x[Stats.SPD] += (r.enhancedStateActive && r.enhancedStateSpdBuff) ? ultSpdBuff : 0
-      x.BREAK_EFFICIENCY_BOOST += (r.enhancedStateActive) ? 0.50 : 0
-      x.DMG_RED_MULTI *= (r.enhancedStateActive && r.talentDmgReductionBuff) ? (1 - talentDmgReductionBuff) : 1
+      x.RES.buff((r.enhancedStateActive) ? talentResBuff : 0, Source.NONE)
+      x.SPD.buff((r.enhancedStateActive && r.enhancedStateSpdBuff) ? ultSpdBuff : 0, Source.NONE)
+      x.BREAK_EFFICIENCY_BOOST.buff((r.enhancedStateActive) ? 0.50 : 0, Source.NONE)
+      x.DMG_RED_MULTI.multiply((r.enhancedStateActive && r.talentDmgReductionBuff) ? (1 - talentDmgReductionBuff) : 1, Source.NONE)
 
       // Should be skill def pen but skill doesnt apply to super break
-      x.DEF_PEN += (e >= 1 && r.e1DefShred && r.enhancedStateActive) ? 0.15 : 0
-      x[Stats.RES] += (e >= 4 && r.e4ResBuff && r.enhancedStateActive) ? 0.50 : 0
-      x.FIRE_RES_PEN += (e >= 6 && r.e6Buffs && r.enhancedStateActive) ? 0.20 : 0
-      x.BREAK_EFFICIENCY_BOOST += (e >= 6 && r.e6Buffs && r.enhancedStateActive) ? 0.50 : 0
+      x.DEF_PEN.buff((e >= 1 && r.e1DefShred && r.enhancedStateActive) ? 0.15 : 0, Source.NONE)
+      x.RES.buff((e >= 4 && r.e4ResBuff && r.enhancedStateActive) ? 0.50 : 0, Source.NONE)
+      x.FIRE_RES_PEN.buff((e >= 6 && r.e6Buffs && r.enhancedStateActive) ? 0.20 : 0, Source.NONE)
+      x.BREAK_EFFICIENCY_BOOST.buff((e >= 6 && r.e6Buffs && r.enhancedStateActive) ? 0.50 : 0, Source.NONE)
 
-      x.BASIC_SCALING += (r.enhancedStateActive) ? basicEnhancedScaling : basicScaling
+      x.BASIC_SCALING.buff((r.enhancedStateActive) ? basicEnhancedScaling : basicScaling, Source.NONE)
 
-      x.BASIC_TOUGHNESS_DMG += (r.enhancedStateActive) ? 45 : 30
-      x.SKILL_TOUGHNESS_DMG += (r.enhancedStateActive) ? 90 : 60
+      x.BASIC_TOUGHNESS_DMG.buff((r.enhancedStateActive) ? 45 : 30, Source.NONE)
+      x.SKILL_TOUGHNESS_DMG.buff((r.enhancedStateActive) ? 90 : 60, Source.NONE)
 
       return x
     },
     finalizeCalculations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r: Conditionals<typeof content> = action.characterConditionals
 
-      buffAbilityVulnerability(x, BREAK_TYPE, ultWeaknessBrokenBreakVulnerability, (r.enhancedStateActive && x.a[Key.ENEMY_WEAKNESS_BROKEN]))
+      buffAbilityVulnerability(x, BREAK_TYPE, (r.enhancedStateActive && x.a[Key.ENEMY_WEAKNESS_BROKEN]) ? ultWeaknessBrokenBreakVulnerability : 0, Source.NONE)
 
-      x.SUPER_BREAK_MODIFIER += (r.superBreakDmg && r.enhancedStateActive && x[Stats.BE] >= 2.00) ? 0.35 : 0
-      x.SUPER_BREAK_MODIFIER += (r.superBreakDmg && r.enhancedStateActive && x[Stats.BE] >= 3.60) ? 0.15 : 0
+      x.SUPER_BREAK_MODIFIER.buff((r.superBreakDmg && r.enhancedStateActive && x.a[Key.BE] >= 2.00) ? 0.35 : 0, Source.NONE)
+      x.SUPER_BREAK_MODIFIER.buff((r.superBreakDmg && r.enhancedStateActive && x.a[Key.BE] >= 3.60) ? 0.15 : 0, Source.NONE)
 
-      x.SKILL_SCALING += (r.enhancedStateActive)
-        ? (0.2 * Math.min(3.60, x[Stats.BE]) + skillEnhancedAtkScaling)
-        : skillScaling
+      x.SKILL_SCALING.buff(
+        (r.enhancedStateActive)
+          ? (0.2 * Math.min(3.60, x.a[Key.BE]) + skillEnhancedAtkScaling)
+          : skillScaling
+        , Source.NONE)
 
-      x.BASIC_DMG += x.BASIC_SCALING * x[Stats.ATK]
-      x.SKILL_DMG += x.SKILL_SCALING * x[Stats.ATK]
+      x.BASIC_DMG.buff(x.a[Key.BASIC_SCALING] * x.a[Key.ATK], Source.NONE)
+      x.SKILL_DMG.buff(x.a[Key.SKILL_SCALING] * x.a[Key.ATK], Source.NONE)
     },
     gpuFinalizeCalculations: (action: OptimizerAction, context: OptimizerContext) => {
       const r: Conditionals<typeof content> = action.characterConditionals

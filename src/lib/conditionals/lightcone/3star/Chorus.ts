b899269c006943d1d6566cc47b3d015d2759a942
Fribbels <fribbelsgithub@gmail.com>
2024-11-05 15:17:25 -0800

    feat: migrate 3* lcs

--- src/lib/conditionals/lightcone/3star/Chorus.ts
+++ src/lib/conditionals/lightcone/3star/Chorus.ts
@@ -1,6 +1,5 @@
 import { Conditionals, ContentDefinition } from 'lib/conditionals/conditionalUtils'
-import { Stats } from 'lib/constants'
-import { ComputedStatsArray } from 'lib/optimizer/computedStatsArray'
+import { ComputedStatsArray, Source } from 'lib/optimizer/computedStatsArray'
 import { TsUtils } from 'lib/TsUtils'
 import { SuperImpositionLevel } from 'types/LightCone'
 import { LightConeConditional } from 'types/LightConeConditionals'
@@ -11,22 +10,31 @@ export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditi
 
   const sValues = [0.08, 0.09, 0.10, 0.11, 0.12]
 
-  const content: ContentDefinition<typeof defaults> = [
-    {
+  const defaults = {
+    inBattleAtkBuff: true,
+  }
+
+  const teammateDefaults = {
+    inBattleAtkBuff: true,
+  }
+
+  const content: ContentDefinition<typeof defaults> = {
+    inBattleAtkBuff: {
       lc: true,
-      id: 'inBattleAtkBuff',
       formItem: 'switch',
+      id: 'inBattleAtkBuff',
       text: t('Content.inBattleAtkBuff.text'),
       content: t('Content.inBattleAtkBuff.content', { AtkBuff: TsUtils.precisionRound(100 * sValues[s]) }),
     },
-  ]
+  }
+  const teammateContent: ContentDefinition<typeof teammateDefaults> = {
+    inBattleAtkBuff: content.inBattleAtkBuff,
+  }
 
   return {
     content: () => Object.values(content),
     teammateContent: () => Object.values(teammateContent),
-    defaults: () => ({
-      inBattleAtkBuff: true,
-    }),
+    defaults: () => defaults,
     teammateDefaults: () => ({
       inBattleAtkBuff: true,
     }),
@@ -35,7 +43,7 @@ export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditi
     precomputeMutualEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const m: Conditionals<typeof teammateContent> = action.lightConeConditionals
 
-      x[Stats.ATK_P] += (m.inBattleAtkBuff) ? sValues[s] : 0
+      x.ATK_P.buff((m.inBattleAtkBuff) ? sValues[s] : 0, Source.NONE)
     },
     finalizeCalculations: () => {
     },
--- src/lib/conditionals/lightcone/3star/Mediation.ts
+++ src/lib/conditionals/lightcone/3star/Mediation.ts
@@ -1,6 +1,5 @@
 import { Conditionals, ContentDefinition } from 'lib/conditionals/conditionalUtils'
-import { Stats } from 'lib/constants'
-import { ComputedStatsArray } from 'lib/optimizer/computedStatsArray'
+import { ComputedStatsArray, Source } from 'lib/optimizer/computedStatsArray'
 import { TsUtils } from 'lib/TsUtils'
 import { SuperImpositionLevel } from 'types/LightCone'
 import { LightConeConditional } from 'types/LightConeConditionals'
@@ -11,31 +10,39 @@ export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditi
 
   const sValues = [12, 14, 16, 18, 20]
 
-  const content: ContentDefinition<typeof defaults> = [
-    {
+  const defaults = {
+    initialSpdBuff: true,
+  }
+
+  const teammateDefaults = {
+    initialSpdBuff: true,
+  }
+
+  const content: ContentDefinition<typeof defaults> = {
+    initialSpdBuff: {
       lc: true,
       id: 'initialSpdBuff',
       formItem: 'switch',
       text: t('Content.initialSpdBuff.text'),
       content: t('Content.initialSpdBuff.content', { SpdBuff: sValues[s] }),
     },
-  ]
+  }
+
+  const teammateContent: ContentDefinition<typeof teammateDefaults> = {
+    initialSpdBuff: content.initialSpdBuff,
+  }
 
   return {
     content: () => Object.values(content),
     teammateContent: () => Object.values(teammateContent),
-    defaults: () => ({
-      initialSpdBuff: true,
-    }),
-    teammateDefaults: () => ({
-      initialSpdBuff: true,
-    }),
+    defaults: () => defaults,
+    teammateDefaults: () => teammateDefaults,
     precomputeEffects: () => {
     },
     precomputeMutualEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const m: Conditionals<typeof teammateContent> = action.lightConeConditionals
 
-      x[Stats.SPD] += (m.initialSpdBuff) ? sValues[s] : 0
+      x.SPD.buff((m.initialSpdBuff) ? sValues[s] : 0, Source.NONE)
     },
     finalizeCalculations: () => {
     },

05bf5e6f6515bf7ca6a8e767cba0bfdac29c6d6b
Fribbels <fribbelsgithub@gmail.com>
2024-11-10 23:28:43 -0800

    refactor: cleanup

--- src/lib/i18n/LanguageSelector.tsx
+++ src/lib/i18n/LanguageSelector.tsx
@@ -4,15 +4,11 @@ import { Assets } from 'lib/rendering/assets'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-// FIXME LOW
-
-type placementOptions = 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topRight'
-
 type Languages = keyof typeof languages
 
 const completedLocales: Languages[] = ['en', 'zh', 'pt'] as const
 
-export function LanguageSelector(props: { style: React.CSSProperties; dropdownStyle: React.CSSProperties; flagOnly: boolean; placement: placementOptions }) {
+export function LanguageSelector() {
   const { i18n } = useTranslation()
   const selectOptions = Object.values(languages)
     .map(({ locale, nativeName, shortName }) => ({
@@ -25,7 +21,6 @@ export function LanguageSelector(props: { style: React.CSSProperties; dropdownSt
       ),
       label: (
         <Flex gap={8}>
-          {/* <img style={{ height: 15, marginTop: 4 }} src={Assets.getFlag(locale)} /> */}
           {nativeName}
           {/* eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access */}
           {completedLocales.includes(locale) ? '' : ' (WIP)'}
@@ -40,11 +35,11 @@ export function LanguageSelector(props: { style: React.CSSProperties; dropdownSt
       onChange={(e: string) => {
         i18n.changeLanguage(e)
       }}
+      style={{ width: 135, marginRight: 6, height: 36 }}
+      placement='bottomLeft'
       optionLabelProp='display'
-      style={props.style}
-      dropdownStyle={props.dropdownStyle}
+      dropdownStyle={{ width: 210 }}
       defaultValue={i18n.resolvedLanguage}
-      placement={props.placement}
       dropdownRender={(menu) => (
         <Flex gap={4} vertical>
           {menu}

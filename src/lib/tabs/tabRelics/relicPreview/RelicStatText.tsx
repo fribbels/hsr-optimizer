d0260a18cb4b51d4ac6367ea7a5dee2f1f70ecc8
FI00ds <lucasrobertson368+github@gmail.com>
2024-11-24 22:31:50 +0000

    feat: language specific styling

--- src/lib/tabs/tabRelics/relicPreview/RelicStatText.tsx
+++ src/lib/tabs/tabRelics/relicPreview/RelicStatText.tsx
@@ -1,11 +1,27 @@
 import { Typography } from 'antd'
+import i18next from 'i18next'
 import styled from 'styled-components'
 
 const { Text } = Typography
 
 const RelicStatText = styled(Text)`
-    font-stretch: 95%;
-    white-space: nowrap;
+    ${generateStyling()}
 `
 
 export default RelicStatText
+
+function generateStyling() {
+  switch (i18next.resolvedLanguage) {
+    case 'fr':
+    case 'pt':
+      return `
+        white-space: nowrap;
+        letter-spacing: -0.2px;
+        font-size: 14px;
+      `
+    default:
+      return `
+        white-space: nowrap;
+      `
+  }
+}

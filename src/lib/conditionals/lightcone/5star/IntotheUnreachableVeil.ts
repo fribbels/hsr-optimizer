804ed16eb85df205d1e82e6d5a3a4cff3a04c89d
Fribbels <fribbelsgithub@gmail.com>
2024-12-02 21:09:29 -0800

    feat: update conditionals

--- src/lib/conditionals/lightcone/5star/IntotheUnreachableVeil.ts
+++ src/lib/conditionals/lightcone/5star/IntotheUnreachableVeil.ts
@@ -11,7 +11,7 @@ import { OptimizerAction, OptimizerContext } from 'types/optimizer'
 export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditionalsController => {
   // const t = TsUtils.wrappedFixedT(withContent).get(null, 'conditionals', 'Lightcones.IntotheUnreachableVeil')
 
-  const sValuesDmgBoost = [0.96, 0.96, 0.96, 0.96, 0.96]
+  const sValuesDmgBoost = [0.48, 0.60, 0.72, 0.84, 0.96]
 
   const defaults = {
     skillUltDmgBoost: true,
@@ -22,7 +22,7 @@ export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditi
       lc: true,
       id: 'skillUltDmgBoost',
       formItem: 'switch',
-      text: 'Skill + Ult DMG boost',
+      text: 'Skill / Ult DMG boost',
       content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
     },
   }

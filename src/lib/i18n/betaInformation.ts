740aa23ad7883c4f09010e7ef2c5999ea36a3e40
FI00ds <lucasrobertson368+github@gmail.com>
2024-12-03 00:15:28 +0000

    feat: 2.7 gameData translations

--- src/lib/i18n/betaInformation.ts
+++ src/lib/i18n/betaInformation.ts
@@ -4,39 +4,102 @@ export const betaInformation: betaInformation = {
   en: {
     Characters: [
       {
-        key: 1225,
+        id: '???',
         value: {
-          Name: 'Fugue',
-          LongName: 'Fugue',
+          Name: 'The Herta',
+          LongName: 'The Herta',
         },
       },
       {
-        key: 1313,
+        id: '???',
         value: {
-          Name: 'Sunday',
-          LongName: 'Sunday',
+          Name: 'Aglaea',
+          LongName: 'Aglaea',
+        },
+      },
+      {
+        id: 8007,
+        value: {
+          Name: 'Caelus',
+          LongName: 'Caelus (Remembrance)',
+        },
+      },
+      {
+        id: 8008,
+        value: {
+          Name: 'Stelle',
+          LongName: 'Stelle (Remembrance)',
         },
       },
     ],
     Lightcones: [
       {
-        id: 23034,
+        id: '????',
         value: {
-          Name: 'A Grounded Ascent',
+          Name: 'Into the Unreachable',
         },
       },
       {
-        id: 23035,
+        id: '????',
         value: {
-          Name: 'Long Road Leads Home',
+          Name: 'Time Woven Into Gold',
+        },
+      },
+      {
+        id: '????',
+        value: {
+          Name: "Geniuses' Greetings",
+        },
+      },
+      {
+        id: '????',
+        value: {
+          Name: 'Sweat Now, Cry Less',
+        },
+      },
+      {
+        id: '????',
+        value: {
+          Name: 'Victory In a Blink',
+        },
+      },
+      {
+        id: '????',
+        value: {
+          Name: 'Shadowburn',
+        },
+      },
+      {
+        id: '????',
+        value: {
+          Name: 'Reminiscence',
         },
       },
     ],
-    RelicSets: [],
+    RelicSets: [
+      {
+        id: '????',
+        value: {
+          Name: 'Hero of Triumphant Song',
+          Description2pc: 'Increases ATK by 12%.',
+          Description4pc: "While the wearer's memosprite is on the field, increases the wearer's SPD by 6%."
+          + " When the wearer's memosprite attacks, increases the wearer and memosprite's CRIT DMG by 30% for 2 turn(s).",
+        },
+      },
+      {
+        id: '????',
+        value: {
+          Name: 'Poet of Mourning Collapse',
+          Description2pc: 'Increases Quantum DMG dealt by 10%.',
+          Description4pc: "Decreases the wearer's SPD by 12%. When entering battle, if the wearer's SPD is less than 110/95,"
+          + " increases the wearer's CRIT Rate by 20%/40%. This effect also applies to the wearer's memosprite.",
+        },
+      },
+    ],
   },
 }
 
 type betaInformation = Partial<Record<InputLocale, { Characters: Character[];Lightcones: LightCone[];RelicSets: RelicSet[] }>>
-type Character = { key: number; value: { Name: string; LongName: string } }
+type Character = { id: number; value: { Name: string; LongName: string } }
 type LightCone = { id: number; value: { Name: string } }
 type RelicSet = { id: number; value: { Name: string; Description2pc: string; Description4pc?: string } }

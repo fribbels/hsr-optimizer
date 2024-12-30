2115251fcd273d3d3b19edf387b373bfc82ff91c
Fribbels <fribbelsgithub@gmail.com>
2024-12-30 00:31:47 -0800

    feat: remembrance lc conditionals

--- src/lib/conditionals/lightcone/3star/Shadowburn.ts
+++ src/lib/conditionals/lightcone/3star/Shadowburn.ts
@@ -1,33 +1,10 @@
-import i18next from 'i18next'
-import { Conditionals, ContentDefinition } from 'lib/conditionals/conditionalUtils'
-import { CURRENT_DATA_VERSION } from 'lib/constants/constants'
-import { ComputedStatsArray } from 'lib/optimization/computedStatsArray'
 import { LightConeConditionalsController } from 'types/conditionals'
-import { SuperImpositionLevel } from 'types/lightCone'
-import { OptimizerAction, OptimizerContext } from 'types/optimizer'
 
-export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditionalsController => {
-  // const t = TsUtils.wrappedFixedT(withContent).get(null, 'conditionals', 'Lightcones.Shadowburn')
-
-  const sValues = [0.00, 0.00, 0.00, 0.00, 0.00]
-
-  const defaults = {}
-
-  const content: ContentDefinition<typeof defaults> = {
-    WIP: {
-      lc: true,
-      id: 'WIP',
-      formItem: 'switch',
-      text: 'WIP',
-      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
-    },
-  }
-
+export default (): LightConeConditionalsController => {
   return {
-    content: () => Object.values(content),
-    defaults: () => defaults,
-    precomputeEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
-      const r = action.lightConeConditionals as Conditionals<typeof content>
+    content: () => [],
+    defaults: () => ({}),
+    precomputeEffects: () => {
     },
     finalizeCalculations: () => {
     },
--- src/lib/conditionals/lightcone/4star/GeniusesGreetings.ts
+++ src/lib/conditionals/lightcone/4star/GeniusesGreetings.ts
@@ -1,7 +1,7 @@
 import i18next from 'i18next'
 import { Conditionals, ContentDefinition } from 'lib/conditionals/conditionalUtils'
 import { CURRENT_DATA_VERSION } from 'lib/constants/constants'
-import { ComputedStatsArray } from 'lib/optimization/computedStatsArray'
+import { ComputedStatsArray, Source } from 'lib/optimization/computedStatsArray'
 import { LightConeConditionalsController } from 'types/conditionals'
 import { SuperImpositionLevel } from 'types/lightCone'
 import { OptimizerAction, OptimizerContext } from 'types/optimizer'
@@ -9,16 +9,18 @@ import { OptimizerAction, OptimizerContext } from 'types/optimizer'
 export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditionalsController => {
   // const t = TsUtils.wrappedFixedT(withContent).get(null, 'conditionals', 'Lightcones.GeniusesGreetings')
 
-  const sValues = [0.00, 0.00, 0.00, 0.00, 0.00]
+  const sValues = [0.20, 0.25, 0.30, 0.35, 0.40]
 
-  const defaults = {}
+  const defaults = {
+    basicDmgBoost: true,
+  }
 
   const content: ContentDefinition<typeof defaults> = {
-    WIP: {
+    basicDmgBoost: {
       lc: true,
-      id: 'WIP',
+      id: 'basicDmgBoost',
       formItem: 'switch',
-      text: 'WIP',
+      text: 'Basic DMG boost',
       content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
     },
   }
@@ -28,6 +30,8 @@ export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditi
     defaults: () => defaults,
     precomputeEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r = action.lightConeConditionals as Conditionals<typeof content>
+
+      x.BASIC_BOOST.buffDual((r.basicDmgBoost) ? sValues[s] : 0, Source.NONE)
     },
     finalizeCalculations: () => {
     },

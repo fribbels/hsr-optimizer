17a70d210616f96f1bb149f19dd765d1add23b50
Fribbels <fribbelsgithub@gmail.com>
2025-01-03 21:14:51 -0800

    feat: character color/card adjustments, 4 star relics rating '?' (#800)
    * feat: color adjustments, aglaea yunli default colors
    
    * feat: bailu centering
    
    * feat: kafka color
    
    * feat: robin color, 4 star rating '?'

--- src/lib/state/metadata.ts
+++ src/lib/state/metadata.ts
@@ -1334,9 +1334,9 @@ function getOverrideImageCenter(): Record<string, {
       z: 1,
     },
     1211: { // Bailu
-      x: 1000,
+      x: 1050,
       y: 950,
-      z: 1,
+      z: 1.05,
     },
     1212: { // Jingliu
       x: 1024,
@@ -1519,9 +1519,9 @@ function getOverrideImageCenter(): Record<string, {
       z: 1.275,
     },
     1402: { // Aglaea
-      x: 1215,
+      x: 1200,
       y: 750,
-      z: 1.15,
+      z: 1.10,
     },
   }
 }

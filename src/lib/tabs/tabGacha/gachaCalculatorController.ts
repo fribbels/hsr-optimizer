a3c01a319a34cf965739b35289807ac6f9a8d778
Fribbels <fribbelsgithub@gmail.com>
2025-01-12 04:15:36 -0800

    feat: updating to probability decimals

--- src/lib/tabs/tabGacha/gachaCalculatorController.ts
+++ src/lib/tabs/tabGacha/gachaCalculatorController.ts
@@ -1,4 +1,4 @@
-import { lightConeDistribution } from 'lib/tabs/tabGacha/gachaRates'
+import { characterCumulative } from 'lib/tabs/tabGacha/gachaRates'
 
 export function simulateWarps() {
   console.log('simulate Warps')
@@ -6,77 +6,92 @@ export function simulateWarps() {
   let wins = 0
   let counts: Record<string, number> = {}
 
-  // for (let i = 0; i < 100000; i++) {
-  //   let e = 0
-  //   let s = 0
-  //   let eGuarantee = false
-  //   let sGuarantee = false
-  //   let eCounter = 0
-  //   let sCounter = 0
-  //
-  //   for (let j = 0; j < 90; j++) {
-  //     if (Math.random() < chance(eCounter)) {
-  //       // Won a 5 star
-  //
-  //       if (eGuarantee) {
-  //         // 50/50 guarantee activated
-  //         e++
-  //         eGuarantee = false
-  //       } else {
-  //         // Roll 50/50
-  //
-  //         if (Math.random() < 0.5625) {
-  //           // Won 50/50
-  //           e++
-  //           eGuarantee = false
-  //         } else {
-  //           // Lost 50/50
-  //           eGuarantee = true
-  //         }
-  //       }
-  //
-  //       eCounter = 0
-  //     } else {
-  //       eCounter++
-  //     }
-  //   }
-  //
-  //   if (counts[e] == null) {
-  //     counts[e] = 0
-  //   }
-  //   counts[e]++
-  // }
-  //
-  // let sum = 0
-  // for (const [key, value] of Object.entries(counts).reverse()) {
-  //   sum = sum + value / 100000 * 100
-  //   counts[key] = sum
-  //
-  //   if (key == '0') {
-  //     console.log(`Fail: ${value / 100000 * 100}%`)
-  //   } else {
-  //     console.log(`E${parseInt(key) - 1}: ${sum}%`)
-  //   }
-  // }
-  const cumulative = []
-  for (let i = 0; i < 80; i++) {
-    cumulative[i] = i == 0 ? lightConeDistribution[i] : cumulative[i - 1] + lightConeDistribution[i]
+  // Adjusted cumulative distribution after 30 pulls
+
+  const n = 10000000
+  for (let i = 0; i < n; i++) {
+    const pity: number = 0
+    // const rand = pity == 0 ? Math.random() * 100 : Math.random() * 100 * (100 - characterCumulative[pity - 1])
+    const rand = Math.random()
+
+    const index = getIndex(rand, characterCumulative, pity)
+
+    if (counts[index] == null) counts[index] = 0
+    counts[index]++
   }
-  console.log(cumulative)
+
+  for (const [key, value] of Object.entries(counts)) {
+    console.log(`${key}: ${value / n * 100}%`)
+  }
+
+  console.log(counts)
 }
 
-// function getIndex(randomNumber) {
-//   let left = 0;
-//   let right = cumulativeDistribution.length - 1;
+function getIndex(random: number, cumulativeDistribution: number[], pity: number) {
+  let left = pity
+  let right = cumulativeDistribution.length - 1
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2)
+    if (random > cumulativeDistribution[mid]) {
+      left = mid + 1
+    } else {
+      right = mid
+    }
+  }
+
+  return left
+}
+
+// for (let i = 0; i < 100000; i++) {
+//   let e = 0
+//   let s = 0
+//   let eGuarantee = false
+//   let sGuarantee = false
+//   let eCounter = 0
+//   let sCounter = 0
 //
-//   while (left < right) {
-//     const mid = Math.floor((left + right) / 2);
-//     if (randomNumber > cumulativeDistribution[mid]) {
-//       left = mid + 1;
+//   for (let j = 0; j < 90; j++) {
+//     if (Math.random() < chance(eCounter)) {
+//       // Won a 5 star
+//
+//       if (eGuarantee) {
+//         // 50/50 guarantee activated
+//         e++
+//         eGuarantee = false
+//       } else {
+//         // Roll 50/50
+//
+//         if (Math.random() < 0.5625) {
+//           // Won 50/50
+//           e++
+//           eGuarantee = false
+//         } else {
+//           // Lost 50/50
+//           eGuarantee = true
+//         }
+//       }
+//
+//       eCounter = 0
 //     } else {
-//       right = mid;
+//       eCounter++
 //     }
 //   }
 //
-//   return left;
+//   if (counts[e] == null) {
+//     counts[e] = 0
+//   }
+//   counts[e]++
+// }
+//
+// let sum = 0
+// for (const [key, value] of Object.entries(counts).reverse()) {
+//   sum = sum + value / 100000 * 100
+//   counts[key] = sum
+//
+//   if (key == '0') {
+//     console.log(`Fail: ${value / 100000 * 100}%`)
+//   } else {
+//     console.log(`E${parseInt(key) - 1}: ${sum}%`)
+//   }
 // }

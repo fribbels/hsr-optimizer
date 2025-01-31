7415e4efdb5fc2824ddfca7658cc407a456a88c1
Wenxchn <wenx.chn@gmail.com>
2025-01-31 00:16:36 -0500

    feat: uid shows/hides on character portrait

--- src/lib/characterPreview/ShowcasePortrait.tsx
+++ src/lib/characterPreview/ShowcasePortrait.tsx
@@ -1,7 +1,12 @@
 import { EditOutlined } from '@ant-design/icons'
 import { Button, ConfigProvider, Flex, Typography } from 'antd'
 import CharacterCustomPortrait from 'lib/characterPreview/CharacterCustomPortrait'
-import { showcaseButtonStyle, showcaseOutline, showcaseShadow, ShowcaseSource } from 'lib/characterPreview/CharacterPreviewComponents'
+import {
+  showcaseButtonStyle,
+  showcaseOutline,
+  showcaseShadow,
+  ShowcaseSource,
+} from 'lib/characterPreview/CharacterPreviewComponents'
 import { ShowcaseDisplayDimensions } from 'lib/characterPreview/characterPreviewController'
 import { parentH, parentW } from 'lib/constants/constantsUi'
 import EditImageModal from 'lib/overlays/modals/EditImageModal'
@@ -15,27 +20,27 @@ import { CustomImageConfig, CustomImagePayload } from 'types/customImage'
 
 const { Text } = Typography
 
-export function ShowcasePortraitLarge() {
-
-}
+export function ShowcasePortraitLarge() {}
 
 export function ShowcasePortrait(props: {
-  source: ShowcaseSource,
-  character: Character,
-  displayDimensions: ShowcaseDisplayDimensions,
+  source: ShowcaseSource
+  character: Character
+  displayDimensions: ShowcaseDisplayDimensions
   customPortrait: CustomImageConfig | undefined
   editPortraitModalOpen: boolean
   setEditPortraitModalOpen: (b: boolean) => void
   onEditPortraitOk: (p: CustomImagePayload) => void
   simScoringResult: SimulationScore
-  artistName: string | undefined,
-  setOriginalCharacterModalInitialCharacter: (c: Character) => void,
-  setOriginalCharacterModalOpen: (b: boolean) => void,
-  setCharacterModalAdd: (b: boolean) => void,
-  onPortraitLoad: (img: string) => void,
+  artistName: string | undefined
+  setOriginalCharacterModalInitialCharacter: (c: Character) => void
+  setOriginalCharacterModalOpen: (b: boolean) => void
+  setCharacterModalAdd: (b: boolean) => void
+  onPortraitLoad: (img: string) => void
 }) {
   const { t } = useTranslation(['charactersTab', 'modals', 'common'])
   const globalThemeConfig = window.store((s) => s.globalThemeConfig)
+  const showcaseUID = window.store((s) => s.savedSession.showcaseUID)
+  const UID = window.store((s) => s.scorerId)
 
   const {
     source,
@@ -78,36 +83,40 @@ export function ShowcasePortrait(props: {
         boxShadow: showcaseShadow,
       }}
     >
-      {
-        (character.portrait || customPortrait)
-          ? (
-            <CharacterCustomPortrait
-              customPortrait={customPortrait ?? character.portrait!}
-              parentW={parentW}
-              onPortraitLoad={onPortraitLoad}
-            />
-          )
-          : (
-            <LoadingBlurredImage
-              src={Assets.getCharacterPortraitById(character.id)}
-              style={{
-                position: 'absolute',
-                left: -charCenter.x * charCenter.z / 2 * tempInnerW / 1024 + parentW / 2,
-                top: -charCenter.y * charCenter.z / 2 * tempInnerW / 1024 + tempParentH / 2,
-                width: tempInnerW * charCenter.z,
-              }}
-              callback={onPortraitLoad}
-            />
-          )
-      }
+      {character.portrait || customPortrait ? (
+        <CharacterCustomPortrait
+          customPortrait={customPortrait ?? character.portrait!}
+          parentW={parentW}
+          onPortraitLoad={onPortraitLoad}
+        />
+      ) : (
+        <LoadingBlurredImage
+          src={Assets.getCharacterPortraitById(character.id)}
+          style={{
+            position: 'absolute',
+            left:
+              (((-charCenter.x * charCenter.z) / 2) * tempInnerW) / 1024 +
+              parentW / 2,
+            top:
+              (((-charCenter.y * charCenter.z) / 2) * tempInnerW) / 1024 +
+              tempParentH / 2,
+            width: tempInnerW * charCenter.z,
+          }}
+          callback={onPortraitLoad}
+        />
+      )}
 
       <ConfigProvider theme={globalThemeConfig}>
-        <Flex vertical style={{ width: 'max-content', marginLeft: 6, marginTop: 6 }} gap={7}>
+        <Flex
+          vertical
+          style={{ width: 'max-content', marginLeft: 6, marginTop: 6 }}
+          gap={7}
+        >
           {source != ShowcaseSource.SHOWCASE_TAB && (
             <Button
               style={showcaseButtonStyle}
               className='character-build-portrait-button'
-              icon={<EditOutlined/>}
+              icon={<EditOutlined />}
               onClick={() => {
                 setCharacterModalAdd(false)
                 setOriginalCharacterModalInitialCharacter(character)
@@ -115,37 +124,37 @@ export function ShowcasePortrait(props: {
               }}
               type='primary'
             >
-              {t('CharacterPreview.EditCharacter')/* Edit character */}
+              {t('CharacterPreview.EditCharacter') /* Edit character */}
             </Button>
           )}
           {source == ShowcaseSource.SHOWCASE_TAB && (
             <Button
               style={showcaseButtonStyle}
               className='character-build-portrait-button'
-              icon={<EditOutlined/>}
+              icon={<EditOutlined />}
               onClick={() => {
                 setOriginalCharacterModalInitialCharacter(character)
                 setOriginalCharacterModalOpen(true)
               }}
               type='primary'
             >
-              {t('CharacterPreview.EditCharacter')/* Edit character */}
+              {t('CharacterPreview.EditCharacter') /* Edit character */}
             </Button>
           )}
           <Button
             style={showcaseButtonStyle}
             className='character-build-portrait-button'
-            icon={<EditOutlined/>}
+            icon={<EditOutlined />}
             onClick={() => setEditPortraitModalOpen(true)}
             type='primary'
           >
-            {t('CharacterPreview.EditPortrait')/* Edit portrait */}
+            {t('CharacterPreview.EditPortrait') /* Edit portrait */}
           </Button>
         </Flex>
       </ConfigProvider>
       <ConfigProvider theme={globalThemeConfig}>
         <EditImageModal
-          title={t('CharacterPreview.EditPortrait')/* Edit portrait */}
+          title={t('CharacterPreview.EditPortrait') /* Edit portrait */}
           aspectRatio={parentW / parentH}
           existingConfig={customPortrait ?? character.portrait}
           open={editPortraitModalOpen}
@@ -158,8 +167,36 @@ export function ShowcasePortrait(props: {
       <Flex
         vertical
         style={{
-          position: 'relative',
-          top: simScoringResult ? tempParentH - 119 : tempParentH - 112,
+          position: 'absolute',
+          bottom: artistName ? 35 : 0,
+          height: 34,
+          paddingLeft: 3,
+          display: showcaseUID ? 'flex' : 'none',
+        }}
+        align='flex-start'
+      >
+        <Text
+          style={{
+            backgroundColor: 'rgb(0 0 0 / 40%)',
+            padding: '4px 12px',
+            borderRadius: 8,
+            fontSize: 14,
+            maxWidth: parentW - 150,
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            whiteSpace: 'nowrap',
+            zIndex: 2,
+            textShadow: '0px 0px 10px black',
+          }}
+        >
+          {UID}
+        </Text>
+      </Flex>
+      <Flex
+        vertical
+        style={{
+          position: 'absolute',
+          bottom: 0,
           height: 34,
           paddingLeft: 3,
           display: artistName ? 'flex' : 'none',
@@ -180,7 +217,11 @@ export function ShowcasePortrait(props: {
             textShadow: '0px 0px 10px black',
           }}
         >
-          {t('CharacterPreview.ArtBy', { artistName: artistName || '' })/* Art by {{artistName}} */}
+          {
+            t('CharacterPreview.ArtBy', {
+              artistName: artistName || '',
+            }) /* Art by {{artistName}} */
+          }
         </Text>
       </Flex>
     </div>

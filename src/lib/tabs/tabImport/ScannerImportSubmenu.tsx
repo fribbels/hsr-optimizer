e8860f413dd2aac1c6750a574d4e00a449a5cd17
Emma <me@its-em.ma>
2025-02-02 19:22:27 -0800

    feat: add checkbox to only import existing characters

--- src/lib/tabs/tabImport/ScannerImportSubmenu.tsx
+++ src/lib/tabs/tabImport/ScannerImportSubmenu.tsx
@@ -1,5 +1,5 @@
 import { UploadOutlined } from '@ant-design/icons'
-import { Button, Divider, Flex, Input, Popconfirm, Steps, Typography, Upload } from 'antd'
+import { Button, Checkbox, Divider, Flex, Input, Popconfirm, Steps, Typography, Upload } from 'antd'
 import { hoyolabParser } from 'lib/importer/hoyoLabFormatParser'
 import { KelzScannerConfig, ScannerSourceToParser, ValidScannerSources } from 'lib/importer/importConfig'
 import { Message } from 'lib/interactions/message'
@@ -33,6 +33,7 @@ export function ScannerImportSubmenu() {
   const [currentCharacters, setCurrentCharacters] = useState<ParsedCharacter[] | undefined>([])
   const [loading1, setLoading1] = useState(false)
   const [loading2, setLoading2] = useState(false)
+  const [onlyImportExisting, setOnlyImportExisting] = useState(false)
   const { t } = useTranslation(['importSaveTab', 'common'])
 
   function beforeUpload(file): Promise<any> {
@@ -133,7 +134,11 @@ export function ScannerImportSubmenu() {
   function mergeCharactersConfirmed() {
     setLoading2(true)
     setTimeout(() => {
-      DB.mergeRelicsWithState(currentRelics, currentCharacters)
+      const charactersToImport = onlyImportExisting 
+        ? currentCharacters?.filter(char => DB.getCharacterById(char.id))
+        : currentCharacters
+
+      DB.mergeRelicsWithState(currentRelics, charactersToImport)
       SaveState.delayedSave()
 
       setTimeout(() => {
@@ -274,6 +279,13 @@ export function ScannerImportSubmenu() {
               characterCount: currentCharacters?.length ?? 0,
             })}
           </Text>
+
+          <Checkbox 
+            checked={onlyImportExisting}
+            onChange={(e) => setOnlyImportExisting(e.target.checked)}
+          >
+            {t('Import.Stage2.CharactersImport.OnlyImportExisting') /* Only import existing characters */}
+          </Checkbox>
 
           <Popconfirm
             title={t('Import.Stage2.CharactersImport.WarningTitle')}

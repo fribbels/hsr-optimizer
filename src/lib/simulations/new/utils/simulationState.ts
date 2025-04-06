f422b670e1215c23fdc97a8f239af13e276a6604
Fribbels <fribbelsgithub@gmail.com>
2025-04-06 13:24:32 -0700

    feat: sim state

--- src/lib/simulations/new/utils/simulationState.ts
+++ src/lib/simulations/new/utils/simulationState.ts
@@ -26,10 +26,66 @@ export type SimulationState = {
 
   // Configuration
   config: {
-    simulationFlags: SimulationFlags
     spdBenchmark?: number
-    mainStatMultiplier: number
+    mainStatMultiplier?: number
+
     overwriteSets: boolean
-    simulationSets: SimulationSets
+    addBreakEffect?: boolean
+    overcapCritRate?: boolean
+    simPoetActive?: boolean
+    characterPoetActive?: boolean
+    forceBasicSpd?: boolean
+    forceBasicSpdValue?: number
+  }
+}
+
+export function createSimulationState(
+  // Required parameters
+  context: OptimizerContext,
+  form: Form,
+  metadata: SimulationMetadata,
+  scoringParams: ScoringParams,
+  simulationSets: SimulationSets,
+  // Optional parameters
+  options: {
+    simulationFlags?: SimulationFlags
+    character?: Character
+    displayRelics?: RelicBuild
+    customMainStats?: SimulationState['inputs']['customMainStats']
+    customTargetSpd?: number
+    spdBenchmark?: number
+    mainStatMultiplier?: number
+    overwriteSets?: boolean
+  } = {},
+): SimulationState {
+  return {
+    inputs: {
+      context,
+      form,
+      metadata,
+      scoringParams,
+      character: options.character,
+      displayRelics: options.displayRelics,
+      customMainStats: options.customMainStats,
+      customTargetSpd: options.customTargetSpd,
+    },
+    config: {
+      spdBenchmark: options.spdBenchmark,
+      mainStatMultiplier: options.mainStatMultiplier ?? 1,
+      overwriteSets: options.overwriteSets ?? false,
+      simulationSets,
+    },
+    results: {},
+    status: {
+      originalBuildCompleted: false,
+      baselineBuildCompleted: false,
+      speedAdjustmentsCompleted: false,
+      benchmarkBuildCompleted: false,
+      perfectBuildCompleted: false,
+      scoreCalculated: false,
+    },
+    metrics: {
+      startTime: performance.now(),
+    },
   }
 }

d93a99d782a11f7b851f42c77cd54d94b03e04ca
Fribbels <fribbelsgithub@gmail.com>
2025-04-20 20:23:21 -0700

    fix: version

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -5,7 +5,7 @@ import gameData from 'data/game_data.json' with { type: 'json' }
 export const CURRENT_OPTIMIZER_VERSION = 'v3.3.1'
 
 // Represents the beta data content version, used for display but not for update notifications
-export const CURRENT_DATA_VERSION = '3.3v2'
+export const CURRENT_DATA_VERSION = '3.3v3'
 
 const generateMessage = (message?: string) => `__VERSION__${message ? ' - ' + message : ''}`
 export const CharacterAnnouncementMessages: Record<string, string> = {

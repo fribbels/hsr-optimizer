1397e0a5735a6523df6aad2cafb62c1142c01ba7
Fribbels <fribbelsgithub@gmail.com>
2025-04-21 20:25:34 -0700

    feat: test utils

---
+++ src/lib/simulations/tests/characters/simKafka.test.ts
@@ -0,0 +1,60 @@
+import { Sets, Stats } from 'lib/constants/constants'
+import { addE6S5Teammate } from 'lib/gpu/tests/webgpuTestGenerator'
+import { toComputedStatsObject } from 'lib/optimization/computedStatsArray'
+import { generateContext } from 'lib/optimization/context/calculateContext'
+import { runStatSimulations } from 'lib/simulations/statSimulation'
+import { Simulation, SimulationRequest, StatSimTypes } from 'lib/simulations/statSimulationTypes'
+import { expectWithinDelta, testStatSpread } from 'lib/simulations/tests/statSimTestUtils'
+import { generateFullDefaultForm } from 'lib/simulations/utils/benchmarkForm'
+import { Metadata } from 'lib/state/metadata'
+import { test } from 'vitest'
+
+Metadata.initialize()
+
+const KAFKA = '1005'
+const LC = '23006'
+
+function initializeBaseRequest() {
+  const form = generateFullDefaultForm(KAFKA, LC, 6, 5)
+  const request: SimulationRequest = {
+    simBody: Stats.ATK_P,
+    simFeet: Stats.SPD,
+    simLinkRope: Stats.ATK_P,
+    simPlanarSphere: Stats.Lightning_DMG,
+    stats: testStatSpread(),
+    simRelicSet1: Sets.PrisonerInDeepConfinement,
+    simRelicSet2: Sets.PrisonerInDeepConfinement,
+    simOrnamentSet: Sets.FirmamentFrontlineGlamoth,
+  }
+}
+
+test('statSim', () => {
+  const form = generateFullDefaultForm(KAFKA, LC, 6, 5)
+  const request: SimulationRequest = {
+    simBody: Stats.CR,
+    simFeet: Stats.SPD,
+    simLinkRope: Stats.ATK_P,
+    simPlanarSphere: Stats.Lightning_DMG,
+    simOrnamentSet: Sets.FirmamentFrontlineGlamoth,
+    simRelicSet1: Sets.PrisonerInDeepConfinement,
+    simRelicSet2: Sets.PrisonerInDeepConfinement,
+    stats: {
+      [Stats.ATK_P]: 3,
+    },
+  }
+
+  addE6S5Teammate(form, 0, '8008', '21051')
+  addE6S5Teammate(form, 1, '1225', '23035')
+  addE6S5Teammate(form, 2, '1313', '23034')
+
+  const simulation: Simulation = {
+    simType: StatSimTypes.SubstatRolls,
+    request: request,
+  } as Simulation
+
+  const context = generateContext(form)
+  const results = runStatSimulations([simulation], form, context, {})
+  const computedStatsObject = toComputedStatsObject(results[0].xa)
+
+  expectWithinDelta(computedStatsObject.COMBO_DMG, 1258999, 1.0)
+})
--- src/lib/simulations/tests/statSimTestUtils.ts
+++ src/lib/simulations/tests/statSimTestUtils.ts
@@ -1,8 +1,27 @@
+import { Stats } from 'lib/constants/constants'
+
 export function expectWithinDelta(actual: number, expected: number, delta: number = 0.001): void {
   const difference = Math.abs(actual - expected)
   const pass = difference <= delta
 
   if (!pass) {
     throw new Error(`Expected ${actual} to be within ${delta} of ${expected} (difference: ${difference})`)
+  }
+}
+
+export function testStatSpread() {
+  return {
+    [Stats.ATK]: 10,
+    [Stats.ATK_P]: 10,
+    [Stats.DEF]: 10,
+    [Stats.DEF_P]: 10,
+    [Stats.HP]: 10,
+    [Stats.HP_P]: 10,
+    [Stats.SPD]: 10,
+    [Stats.CR]: 10,
+    [Stats.CD]: 10,
+    [Stats.EHR]: 10,
+    [Stats.RES]: 10,
+    [Stats.BE]: 10,
   }
 }

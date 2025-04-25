9d50af072bd6ff24e8b038813a710130e4437631
Fribbels <fribbelsgithub@gmail.com>
2025-04-25 00:00:18 -0700

    feat: add more custom benchmark tests, err test

--- src/lib/simulations/tests/customBenchmark/benchmarkOrchestrator.test.ts
+++ src/lib/simulations/tests/customBenchmark/benchmarkOrchestrator.test.ts
@@ -1,7 +1,30 @@
 import { Sets, Stats } from 'lib/constants/constants'
 import { expectBenchmarkResultsToMatch } from 'lib/simulations/tests/customBenchmark/benchmarkOrchestratorTestUtils'
 import { generateE6S5Test, testCharacter, testMains, testSets, testStatSpread } from 'lib/simulations/tests/simTestUtils'
-import { BLACK_SWAN, HUOHUO, KAFKA, NIGHT_OF_FRIGHT, PAST_SELF_IN_MIRROR, PATIENCE_IS_ALL_YOU_NEED, REFORGED_REMEMBRANCE, RUAN_MEI } from 'lib/simulations/tests/testMetadataConstants'
+import {
+  A_GROUNDED_ASCENT,
+  BLACK_SWAN,
+  FIREFLY,
+  FLOWING_NIGHTGLOW,
+  HUOHUO,
+  INTO_THE_UNREACHABLE_VEIL,
+  KAFKA,
+  LINGSHA,
+  MEMORIES_OF_THE_PAST,
+  NIGHT_OF_FRIGHT,
+  PAST_SELF_IN_MIRROR,
+  PATIENCE_IS_ALL_YOU_NEED,
+  REFORGED_REMEMBRANCE,
+  ROBIN,
+  RUAN_MEI,
+  SCENT_ALONE_STAYS_TRUE,
+  STELLE_HARMONY,
+  STELLE_REMEMBRANCE,
+  SUNDAY,
+  SWEAT_NOW_CRY_LESS,
+  THE_HERTA,
+  WHEREABOUTS_SHOULD_DREAMS_REST,
+} from 'lib/simulations/tests/testMetadataConstants'
 import { Metadata } from 'lib/state/metadata'
 import { test } from 'vitest'
 
@@ -58,128 +81,57 @@ test('Kafka benchmark 200', async () => {
   )
 })
 
-// test('Acheron benchmark', async () => {
-//   await expectBenchmarkResultsToMatch(
-//     generateE6S5Test({
-//       character: testCharacter(ACHERON, ALONG_THE_PASSING_SHORE),
-//       teammate0: testCharacter(PELA, RESOLUTION_SHINES_AS_PEARLS_OF_SWEAT),
-//       teammate1: testCharacter(SILVER_WOLF, INCESSANT_RAIN),
-//       teammate2: testCharacter(AVENTURINE, TREND_OF_THE_UNIVERSAL_MARKET),
-//       sets: testSets(Sets.PioneerDiverOfDeadWaters, Sets.PioneerDiverOfDeadWaters, Sets.IzumoGenseiAndTakamaDivineRealm),
-//       mains: testMains(Stats.CD, Stats.ATK_P, Stats.Lightning_DMG, Stats.ATK_P),
-//       stats: testStatSpread(),
-//     }),
-//     1.2527343791677188,
-//   )
-// })
-//
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// test('Castorice benchmark poet 113.4', async () => {
-//   await expectBenchmarkResultsToMatch(
-//     generateE6S5Test({
-//       character: testCharacter(CASTORICE, MAKE_FAREWELLS_MORE_BEAUTIFUL),
-//       teammate0: testCharacter(TRIBBIE, IF_TIME_WERE_A_FLOWER),
-//       teammate1: testCharacter(STELLE_REMEMBRANCE, VICTORY_IN_A_BLINK),
-//       teammate2: testCharacter(LINGSHA, SCENT_ALONE_STAYS_TRUE),
-//       sets: testSets(Sets.PoetOfMourningCollapse, Sets.PoetOfMourningCollapse, Sets.BoneCollectionsSereneDemesne),
-//       mains: testMains(Stats.CD, Stats.HP_P, Stats.Quantum_DMG, Stats.HP_P),
-//       stats: testStatSpread(),
-//     }),
-//     0.7115087301725886,
-//   )
-// })
-//
-// test('Castorice benchmark poet 100.4', async () => {
-//   await expectBenchmarkResultsToMatch(
-//     generateE6S5Test({
-//       character: testCharacter(CASTORICE, MAKE_FAREWELLS_MORE_BEAUTIFUL),
-//       teammate0: testCharacter(TRIBBIE, IF_TIME_WERE_A_FLOWER),
-//       teammate1: testCharacter(STELLE_REMEMBRANCE, VICTORY_IN_A_BLINK),
-//       teammate2: testCharacter(LINGSHA, SCENT_ALONE_STAYS_TRUE),
-//       sets: testSets(Sets.PoetOfMourningCollapse, Sets.PoetOfMourningCollapse, Sets.BoneCollectionsSereneDemesne),
-//       mains: testMains(Stats.CD, Stats.HP_P, Stats.Quantum_DMG, Stats.HP_P),
-//       stats: testStatSpreadSpd(5),
-//     }),
-//     0.9678187582469107,
-//   )
-// })
-//
-// test('Castorice benchmark poet 87.4', async () => {
-//   await expectBenchmarkResultsToMatch(
-//     generateE6S5Test({
-//       character: testCharacter(CASTORICE, MAKE_FAREWELLS_MORE_BEAUTIFUL),
-//       teammate0: testCharacter(TRIBBIE, IF_TIME_WERE_A_FLOWER),
-//       teammate1: testCharacter(STELLE_REMEMBRANCE, VICTORY_IN_A_BLINK),
-//       teammate2: testCharacter(LINGSHA, SCENT_ALONE_STAYS_TRUE),
-//       sets: testSets(Sets.PoetOfMourningCollapse, Sets.PoetOfMourningCollapse, Sets.BoneCollectionsSereneDemesne),
-//       mains: testMains(Stats.CD, Stats.HP_P, Stats.Quantum_DMG, Stats.HP_P),
-//       stats: testStatSpreadSpd(0),
-//     }),
-//     1.145411776491712,
-//   )
-// })
-//
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// test('Castorice benchmark longevous 121', async () => {
-//   await expectBenchmarkResultsToMatch(
-//     generateE6S5Test({
-//       character: testCharacter(CASTORICE, MAKE_FAREWELLS_MORE_BEAUTIFUL),
-//       teammate0: testCharacter(TRIBBIE, IF_TIME_WERE_A_FLOWER),
-//       teammate1: testCharacter(STELLE_REMEMBRANCE, VICTORY_IN_A_BLINK),
-//       teammate2: testCharacter(LINGSHA, SCENT_ALONE_STAYS_TRUE),
-//       sets: testSets(Sets.LongevousDisciple, Sets.LongevousDisciple, Sets.BoneCollectionsSereneDemesne),
-//       mains: testMains(Stats.CD, Stats.HP_P, Stats.Quantum_DMG, Stats.HP_P),
-//       stats: testStatSpread(),
-//     }),
-//     0.7295679289358679,
-//   )
-// })
-//
-// test('Castorice benchmark longevous 108', async () => {
-//   await expectBenchmarkResultsToMatch(
-//     generateE6S5Test({
-//       character: testCharacter(CASTORICE, MAKE_FAREWELLS_MORE_BEAUTIFUL),
-//       teammate0: testCharacter(TRIBBIE, IF_TIME_WERE_A_FLOWER),
-//       teammate1: testCharacter(STELLE_REMEMBRANCE, VICTORY_IN_A_BLINK),
-//       teammate2: testCharacter(LINGSHA, SCENT_ALONE_STAYS_TRUE),
-//       sets: testSets(Sets.LongevousDisciple, Sets.LongevousDisciple, Sets.BoneCollectionsSereneDemesne),
-//       mains: testMains(Stats.CD, Stats.HP_P, Stats.Quantum_DMG, Stats.HP_P),
-//       stats: testStatSpreadSpd(5),
-//     }),
-//     0.7295679289358679,
-//   )
-// })
-//
-// test('Castorice benchmark longevous 95', async () => {
-//   await expectBenchmarkResultsToMatch(
-//     generateE6S5Test({
-//       character: testCharacter(CASTORICE, MAKE_FAREWELLS_MORE_BEAUTIFUL),
-//       teammate0: testCharacter(TRIBBIE, IF_TIME_WERE_A_FLOWER),
-//       teammate1: testCharacter(STELLE_REMEMBRANCE, VICTORY_IN_A_BLINK),
-//       teammate2: testCharacter(LINGSHA, SCENT_ALONE_STAYS_TRUE),
-//       sets: testSets(Sets.LongevousDisciple, Sets.LongevousDisciple, Sets.BoneCollectionsSereneDemesne),
-//       mains: testMains(Stats.CD, Stats.HP_P, Stats.Quantum_DMG, Stats.HP_P),
-//       stats: testStatSpreadSpd(0),
-//     }),
-//     0.7295679289358679,
-//   )
-// })
-//
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// test('Tribbie benchmark poet 88.3', async () => {
-//   await expectBenchmarkResultsToMatch(
-//     generateE6S5Test({
-//       character: testCharacter(TRIBBIE, IF_TIME_WERE_A_FLOWER),
-//       teammate0: testCharacter(THE_HERTA, INTO_THE_UNREACHABLE_VEIL),
-//       teammate1: testCharacter(SERVAL, PASSKEY),
-//       teammate2: testCharacter(LINGSHA, SCENT_ALONE_STAYS_TRUE),
-//       sets: testSets(Sets.PoetOfMourningCollapse, Sets.PoetOfMourningCollapse, Sets.BoneCollectionsSereneDemesne),
-//       mains: testMains(Stats.CD, Stats.HP_P, Stats.Quantum_DMG, Stats.HP_P),
-//       stats: testStatSpreadSpd(0),
-//     }),
-//     0.9012144701511204,
-//   )
-// })
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+test('Firefly benchmark 160', async () => {
+  await expectBenchmarkResultsToMatch(
+    160,
+    generateE6S5Test({
+      character: testCharacter(FIREFLY, WHEREABOUTS_SHOULD_DREAMS_REST),
+      teammate0: testCharacter(STELLE_HARMONY, MEMORIES_OF_THE_PAST),
+      teammate1: testCharacter(RUAN_MEI, PAST_SELF_IN_MIRROR),
+      teammate2: testCharacter(LINGSHA, SCENT_ALONE_STAYS_TRUE),
+      sets: testSets(Sets.IronCavalryAgainstTheScourge, Sets.IronCavalryAgainstTheScourge, Sets.ForgeOfTheKalpagniLantern),
+      mains: testMains(Stats.ATK_P, Stats.SPD, Stats.Lightning_DMG, Stats.ATK_P),
+      stats: testStatSpread(),
+    }),
+    3765406,
+    4595097.5,
+  )
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+test('The herta benchmark 100', async () => {
+  await expectBenchmarkResultsToMatch(
+    100,
+    generateE6S5Test({
+      character: testCharacter(THE_HERTA, INTO_THE_UNREACHABLE_VEIL),
+      teammate0: testCharacter(STELLE_REMEMBRANCE, SWEAT_NOW_CRY_LESS),
+      teammate1: testCharacter(SUNDAY, A_GROUNDED_ASCENT),
+      teammate2: testCharacter(ROBIN, FLOWING_NIGHTGLOW),
+      sets: testSets(Sets.ScholarLostInErudition, Sets.ScholarLostInErudition, Sets.IzumoGenseiAndTakamaDivineRealm),
+      mains: testMains(Stats.ATK_P, Stats.SPD, Stats.Lightning_DMG, Stats.ATK_P),
+      stats: testStatSpread(),
+    }),
+    2837405,
+    3546020.5,
+  )
+})
+
+test('The herta benchmark 100 err', async () => {
+  await expectBenchmarkResultsToMatch(
+    100,
+    generateE6S5Test({
+      character: testCharacter(THE_HERTA, INTO_THE_UNREACHABLE_VEIL),
+      teammate0: testCharacter(STELLE_REMEMBRANCE, SWEAT_NOW_CRY_LESS),
+      teammate1: testCharacter(SUNDAY, A_GROUNDED_ASCENT),
+      teammate2: testCharacter(ROBIN, FLOWING_NIGHTGLOW),
+      sets: testSets(Sets.ScholarLostInErudition, Sets.ScholarLostInErudition, Sets.IzumoGenseiAndTakamaDivineRealm),
+      mains: testMains(Stats.ATK_P, Stats.SPD, Stats.Lightning_DMG, Stats.ERR),
+      stats: testStatSpread(),
+    }),
+    2608734.5,
+    3268191,
+  )
+})

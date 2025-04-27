98e495088f04b4869282ce2e48a97c1bbf10ce42
Fribbels <fribbelsgithub@gmail.com>
2025-04-27 02:44:01 -0700

    fix: reregister conditionals in initializeContextConditionals for worker transfers

--- src/lib/simulations/contextConditionals.ts
+++ src/lib/simulations/contextConditionals.ts
@@ -1,7 +1,7 @@
 import { CharacterConditionalsResolver } from 'lib/conditionals/resolver/characterConditionalsResolver'
 import { LightConeConditionalsResolver } from 'lib/conditionals/resolver/lightConeConditionalsResolver'
 import { DynamicConditional } from 'lib/gpu/conditionals/dynamicConditionals'
-import { wrapTeammateDynamicConditional } from 'lib/optimization/calculateConditionals'
+import { calculateContextConditionalRegistry, wrapTeammateDynamicConditional } from 'lib/optimization/calculateConditionals'
 import { CharacterMetadata, OptimizerAction, OptimizerContext } from 'types/optimizer'
 
 export function initializeContextConditionals(context: OptimizerContext) {
@@ -17,6 +17,8 @@ export function initializeContextConditionals(context: OptimizerContext) {
     // Reconstruct arrays after transfer
     action.precomputedX.a = new Float32Array(Object.values(action.precomputedX.a))
     action.precomputedM.a = new Float32Array(Object.values(action.precomputedM.a))
+
+    calculateContextConditionalRegistry(action, context, context.characterConditionalController, context.lightConeConditionalController)
   }
 }
 

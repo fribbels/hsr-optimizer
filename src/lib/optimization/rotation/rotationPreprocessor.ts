e395f5f6cda22c1b052c34aac95961f3d031b457
Fribbels <fribbelsgithub@gmail.com>
2025-05-02 20:24:15 -0700

    feat: add new set preprocessors

--- src/lib/optimization/rotation/rotationPreprocessor.ts
+++ src/lib/optimization/rotation/rotationPreprocessor.ts
@@ -1,5 +1,6 @@
 import { Sets } from 'lib/constants/constants'
-import { AbilityKind, toTurnAbility, TurnAbility } from 'lib/optimization/rotation/abilityConfig'
+import { AbilityKind, toTurnAbility, TurnAbility, TurnMarker } from 'lib/optimization/rotation/abilityConfig'
+import { CASTORICE } from 'lib/simulations/tests/testMetadataConstants'
 import {
   ComboBooleanConditional,
   ComboNumberConditional,
@@ -8,106 +9,121 @@ import {
 import { Form } from 'types/form'
 import { preprocessTurnAbilities } from './turnPreprocessor'
 
+abstract class AbilityPreprocessorBase {
+  abstract id: string
+  abstract reset(): void
+  abstract processAbility(turnAbility: TurnAbility, index: number, comboState: ComboState): void
+}
+
 /**
  * Some passives such as Scholar Lost In Erudition set only activate after abilities trigger them.
  * Use the simulation ability rotation to precompute when their activations are active.
  */
 export function precomputeConditionalActivations(comboState: ComboState, request: Form) {
-  const setPreprocessors = [
-    scholarLostInEruditionPreprocessor(comboState, request),
-    // Hunter of Glacial Forest
-    // Firesmith of Lava-Forging
-    // Band of Sizzling Thunder
-  ]
+  const filteredSetPreprocessors = setPreprocessors
+  const filteredCharacterPreprocessors = characterPreprocessors.filter((x) => x.id == request.characterId)
 
-  const characterPreprocessors = [
-    castoricePreprocessor(comboState, request),
-  ].filter((x) => x.id == request.characterId)
+  for (const pre of filteredSetPreprocessors) pre.reset()
+  for (const pre of filteredCharacterPreprocessors) pre.reset()
 
   const comboAbilities = preprocessTurnAbilities(comboState.comboTurnAbilities.map(toTurnAbility))
   for (let i = 1; i < comboAbilities.length; i++) {
     const turnAbility = comboAbilities[i]
-
-    for (const preprocessor of setPreprocessors) {
-      preprocessor.processAbility(turnAbility, i)
+    for (const preprocessor of filteredSetPreprocessors) {
+      preprocessor.processAbility(turnAbility, i, comboState)
     }
-
-    for (const preprocessor of characterPreprocessors) {
-      preprocessor.processAbility(turnAbility, i)
+    for (const preprocessor of filteredCharacterPreprocessors) {
+      preprocessor.processAbility(turnAbility, i, comboState)
     }
   }
 }
 
-type AbilityPreprocessor = {
-  id: string
-  state: Record<string, boolean | number>
-  processAbility: (turnAbility: TurnAbility, index: number) => void
-}
+class ScholarLostInEruditionPreprocessor extends AbilityPreprocessorBase {
+  id = Sets.ScholarLostInErudition
+  defaultState = { scholarActivated: false }
+  state = { ...this.defaultState }
 
-function scholarLostInEruditionPreprocessor(comboState: ComboState, request: Form): AbilityPreprocessor {
-  return {
-    id: Sets.ScholarLostInErudition,
-    state: {
-      scholarActivated: false,
-    },
-    processAbility: function (turnAbility: TurnAbility, index: number) {
-      const { kind, marker } = turnAbility
+  reset() {
+    this.state = { ...this.defaultState }
+  }
 
-      if (kind == AbilityKind.ULT) {
-        this.state.scholarActivated = true
-      }
-
-      if (kind == AbilityKind.SKILL && this.state.scholarActivated) {
-        this.state.scholarActivated = false
-        setComboBooleanCategorySetActivation(comboState, Sets.ScholarLostInErudition, index, true)
-      } else {
-        setComboBooleanCategorySetActivation(comboState, Sets.ScholarLostInErudition, index, false)
-      }
-    },
+  processAbility(turnAbility: TurnAbility, index: number, comboState: ComboState) {
+    const { kind } = turnAbility
+    if (kind == AbilityKind.ULT) {
+      this.state.scholarActivated = true
+    }
+    if (kind == AbilityKind.SKILL && this.state.scholarActivated) {
+      this.state.scholarActivated = false
+      setComboBooleanCategorySetActivation(comboState, Sets.ScholarLostInErudition, index, true)
+    } else {
+      setComboBooleanCategorySetActivation(comboState, Sets.ScholarLostInErudition, index, false)
+    }
   }
 }
 
-function castoricePreprocessor(comboState: ComboState, request: Form): AbilityPreprocessor {
-  return {
-    id: '1407',
-    state: {
-      memoSkillEnhances: 3,
-      memoDmgStacks: 0,
-      e2Activated: false,
-    },
-    processAbility: function (turnAbility: TurnAbility, index: number) {
-      const { kind, marker } = turnAbility
-      // E1
+class WavestriderCaptainPreprocessor extends AbilityPreprocessorBase {
+  id = Sets.WavestriderCaptain
+  defaultState = { buffTurnsLeft: 0 }
+  state = { ...this.defaultState }
 
-      let memoDmgStacks = this.state.memoDmgStacks as number
-      if (kind == AbilityKind.MEMO_SKILL) {
-        const value = memoDmgStacks + 1
-        setComboNumberCategoryCharacterActivation(comboState, 'memoDmgStacks', index, value)
-        setComboNumberCategoryCharacterActivation(comboState, 'memoSkillEnhances', index, Math.min(3, value))
-        memoDmgStacks = Math.min(6, memoDmgStacks + 1)
-      } else if (kind == AbilityKind.MEMO_TALENT) {
-        setComboNumberCategoryCharacterActivation(comboState, 'memoDmgStacks', index, memoDmgStacks)
-        setComboNumberCategoryCharacterActivation(comboState, 'memoSkillEnhances', index, 1)
-      } else {
-        memoDmgStacks = 0
-        setComboNumberCategoryCharacterActivation(comboState, 'memoDmgStacks', index, memoDmgStacks)
-        setComboNumberCategoryCharacterActivation(comboState, 'memoSkillEnhances', index, 1)
-      }
-      this.state.memoDmgStacks = memoDmgStacks
+  reset() {
+    this.state = { ...this.defaultState }
+  }
 
-      // E2
+  processAbility(turnAbility: TurnAbility, index: number, comboState: ComboState) {
+    const { kind, marker } = turnAbility
+    if (kind === AbilityKind.ULT) {
+      this.state.buffTurnsLeft = 2
+    }
+    setComboBooleanCategorySetActivation(
+      comboState,
+      Sets.WavestriderCaptain,
+      index,
+      this.state.buffTurnsLeft > 0,
+    )
+    if (marker === TurnMarker.END || marker === TurnMarker.WHOLE) {
+      this.state.buffTurnsLeft = Math.max(this.state.buffTurnsLeft - 1, 0)
+    }
+  }
+}
 
-      if (kind == AbilityKind.ULT) {
-        this.state.e2Activated = true
-      }
+class CastoricePreprocessor extends AbilityPreprocessorBase {
+  id = CASTORICE
+  defaultState = { memoSkillEnhances: 3, memoDmgStacks: 0, e2Activated: false }
+  state = { ...this.defaultState }
 
-      if (kind == AbilityKind.MEMO_SKILL && this.state.e2Activated) {
-        this.state.e2Activated = false
-        setComboBooleanCategoryCharacterActivation(comboState, 'e2MemoSkillDmgBoost', index, true)
-      } else {
-        setComboBooleanCategoryCharacterActivation(comboState, 'e2MemoSkillDmgBoost', index, false)
-      }
-    },
+  reset() {
+    this.state = { ...this.defaultState }
+  }
+
+  processAbility(turnAbility: TurnAbility, index: number, comboState: ComboState) {
+    const { kind } = turnAbility
+    // E1
+    let memoDmgStacks = this.state.memoDmgStacks
+    if (kind == AbilityKind.MEMO_SKILL) {
+      const value = memoDmgStacks + 1
+      setComboNumberCategoryCharacterActivation(comboState, 'memoDmgStacks', index, value)
+      setComboNumberCategoryCharacterActivation(comboState, 'memoSkillEnhances', index, Math.min(3, value))
+      memoDmgStacks = Math.min(6, memoDmgStacks + 1)
+    } else if (kind == AbilityKind.MEMO_TALENT) {
+      setComboNumberCategoryCharacterActivation(comboState, 'memoDmgStacks', index, memoDmgStacks)
+      setComboNumberCategoryCharacterActivation(comboState, 'memoSkillEnhances', index, 1)
+    } else {
+      memoDmgStacks = 0
+      setComboNumberCategoryCharacterActivation(comboState, 'memoDmgStacks', index, memoDmgStacks)
+      setComboNumberCategoryCharacterActivation(comboState, 'memoSkillEnhances', index, 1)
+    }
+    this.state.memoDmgStacks = memoDmgStacks
+    // E2
+    if (kind == AbilityKind.ULT) {
+      this.state.e2Activated = true
+    }
+    if (kind == AbilityKind.MEMO_SKILL && this.state.e2Activated) {
+      this.state.e2Activated = false
+      setComboBooleanCategoryCharacterActivation(comboState, 'e2MemoSkillDmgBoost', index, true)
+    } else {
+      setComboBooleanCategoryCharacterActivation(comboState, 'e2MemoSkillDmgBoost', index, false)
+    }
   }
 }
 
@@ -143,3 +159,12 @@ function setComboNumberCategoryCharacterActivation(comboState: ComboState, condi
     category.partitions = category.partitions.sort((a, b) => a.value - b.value)
   }
 }
+
+const setPreprocessors: AbilityPreprocessorBase[] = [
+  new ScholarLostInEruditionPreprocessor(),
+  new WavestriderCaptainPreprocessor(),
+]
+
+const characterPreprocessors: AbilityPreprocessorBase[] = [
+  new CastoricePreprocessor(),
+]

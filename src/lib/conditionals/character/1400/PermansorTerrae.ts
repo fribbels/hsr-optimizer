876ec9671c04a6e3c77a0b94dc4a153a888c8cad
Fribbels <fribbelsgithub@gmail.com>
2025-08-14 10:37:19 -0700

    hotfix: dhpt bondmate should trigger summon based conditionals

--- src/lib/conditionals/character/1400/PermansorTerrae.ts
+++ src/lib/conditionals/character/1400/PermansorTerrae.ts
@@ -117,6 +117,11 @@ export default (e: Eidolon, withContent: boolean): CharacterConditionalsControll
 
       x.SUMMONS.set(1, SOURCE_TALENT)
     },
+    initializeTeammateConfigurations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
+      const t = action.characterConditionals as Conditionals<typeof content>
+
+      x.SUMMONS.buff((t.bondmate) ? 1 : 0, SOURCE_SKILL)
+    },
     precomputeEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r = action.characterConditionals as Conditionals<typeof content>
 

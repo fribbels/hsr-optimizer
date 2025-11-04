c9af18b6b0ec8a532cae32d4526c2e6cbc41ba4c
Fribbels <fribbelsgithub@gmail.com>
2025-11-03 20:07:38 -0800

    feat: initial conditionals

--- src/lib/conditionals/character/1300/TheDahlia.ts
+++ src/lib/conditionals/character/1300/TheDahlia.ts
@@ -1,6 +1,8 @@
 import {
   AbilityType,
+  ASHBLAZING_ATK_STACK,
   BREAK_DMG_TYPE,
+  FUA_DMG_TYPE,
 } from 'lib/conditionals/conditionalConstants'
 import {
   AbilityEidolon,
@@ -9,12 +11,31 @@ import {
 } from 'lib/conditionals/conditionalUtils'
 import { Source } from 'lib/optimization/buffSource'
 import {
+  buffAbilityCd,
   buffAbilityDmg,
   Target,
 } from 'lib/optimization/calculateBuffs'
 import { ComputedStatsArray } from 'lib/optimization/computedStatsArray'
 import { TsUtils } from 'lib/utils/TsUtils'
 
+import i18next from 'i18next'
+import {
+  boostAshblazingAtkP,
+  gpuBoostAshblazingAtkP,
+  gpuStandardAdditionalDmgAtkFinalizer,
+  standardAdditionalDmgAtkFinalizer,
+} from 'lib/conditionals/conditionalFinalizers'
+import {
+  dynamicStatConversion,
+  gpuDynamicStatConversion,
+} from 'lib/conditionals/evaluation/statConversion'
+import {
+  ConditionalActivation,
+  ConditionalType,
+  CURRENT_DATA_VERSION,
+  Stats,
+} from 'lib/constants/constants'
+import { wgslTrue } from 'lib/gpu/injection/wgslUtils'
 import { THE_DAHLIA } from 'lib/simulations/tests/testMetadataConstants'
 import { Eidolon } from 'types/character'
 import { CharacterConditionalsController } from 'types/conditionals'
@@ -25,7 +46,7 @@ import {
 
 export default (e: Eidolon, withContent: boolean): CharacterConditionalsController => {
   // const t = TsUtils.wrappedFixedT(withContent).get(null, 'conditionals', 'Characters.TheDahlia')
-  const { basic, skill, ult, talent } = AbilityEidolon.SKILL_BASIC_3_ULT_TALENT_5
+  const { basic, skill, ult, talent } = AbilityEidolon.ULT_BASIC_3_SKILL_TALENT_5
   const {
     SOURCE_BASIC,
     SOURCE_SKILL,
@@ -40,92 +61,123 @@ export default (e: Eidolon, withContent: boolean): CharacterConditionalsControll
     SOURCE_E6,
   } = Source.character(THE_DAHLIA)
 
-  const skillBeValue = skill(e, 0.30, 0.33)
-  const skillDefPenValue = skill(e, 0.18, 0.20)
-
   const basicScaling = basic(e, 1.00, 1.10)
-  const ultScaling = ult(e, 2.00, 2.20)
-  const superBreakScaling = talent(e, 1.00, 1.10)
+  const skillScaling = skill(e, 1.60, 1.76)
+  const ultScaling = ult(e, 3.00, 3.24)
+
+  const fuaScaling = talent(e, 0.30, 0.33)
+  const fuaSuperBreakScaling = talent(e, 2.00, 2.20)
+
+  const ultDefPenValue = ult(e, 0.18, 0.20)
+
+  const superBreakScaling = talent(e, 0.60, 0.66)
 
   const defaults = {
-    // torridScorch: true,
-    // foxianPrayer: false,
-    // defReduction: true,
-    // superBreakDmg: true,
-    // e4BreakDmg: true,
-    // e6BreakEfficiency: true,
+    zoneActive: true,
+    ultDefPen: true,
+    dancePartner: false,
+    spdBuff: true,
+    e1Buffs: true,
+    e2ResPen: true,
+    e4Vuln: true,
+    e6BeBuff: true,
   }
 
   const teammateDefaults = {
-    // foxianPrayer: true,
-    // be220Buff: true,
-    // weaknessBreakBeStacks: 2,
-    // defReduction: true,
-    // superBreakDmg: true,
-    // e4BreakDmg: true,
+    zoneActive: true,
+    ultDefPen: true,
+    dancePartner: true,
+    beConversion: true,
+    teammateBeValue: 3.00,
+    spdBuff: true,
+    e1Buffs: true,
+    e2ResPen: true,
+    e4Vuln: true,
   }
 
   const content: ContentDefinition<typeof defaults> = {
-    // torridScorch: {
-    //   id: 'torridScorch',
-    //   formItem: 'switch',
-    //   text: t('Content.torridScorch.text'),
-    //   content: t('Content.torridScorch.content'),
-    // },
-    // foxianPrayer: {
-    //   id: 'foxianPrayer',
-    //   formItem: 'switch',
-    //   text: t('Content.foxianPrayer.text'),
-    //   content: t('Content.foxianPrayer.content', { BreakBuff: TsUtils.precisionRound(100 * skillBeValue) }),
-    // },
-    // defReduction: {
-    //   id: 'defReduction',
-    //   formItem: 'switch',
-    //   text: t('Content.defReduction.text'),
-    //   content: t('Content.defReduction.content', { DefShred: TsUtils.precisionRound(100 * skillDefPenValue) }),
-    // },
-    // superBreakDmg: {
-    //   id: 'superBreakDmg',
-    //   formItem: 'switch',
-    //   text: t('Content.superBreakDmg.text'),
-    //   content: t('Content.superBreakDmg.content', { SuperBreakMultiplier: TsUtils.precisionRound(100 * superBreakScaling) }),
-    // },
-    // e4BreakDmg: {
-    //   id: 'e4BreakDmg',
-    //   formItem: 'switch',
-    //   text: t('Content.e4BreakDmg.text'),
-    //   content: t('Content.e4BreakDmg.content'),
-    //   disabled: e < 4,
-    // },
-    // e6BreakEfficiency: {
-    //   id: 'e6BreakEfficiency',
-    //   formItem: 'switch',
-    //   text: t('Content.e6BreakEfficiency.text'),
-    //   content: t('Content.e6BreakEfficiency.content'),
-    //   disabled: e < 6,
-    // },
+    zoneActive: {
+      id: 'zoneActive',
+      formItem: 'switch',
+      text: 'Zone active',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+    },
+    ultDefPen: {
+      id: 'ultDefPen',
+      formItem: 'switch',
+      text: 'Ult DEF PEN',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+    },
+    dancePartner: {
+      id: 'dancePartner',
+      formItem: 'switch',
+      text: 'Dance Partner',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+    },
+    spdBuff: {
+      id: 'spdBuff',
+      formItem: 'switch',
+      text: 'SPD buff',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+    },
+    e1Buffs: {
+      id: 'e1Buffs',
+      formItem: 'switch',
+      text: 'E1 buffs',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+      disabled: e < 6,
+    },
+    e2ResPen: {
+      id: 'e2ResPen',
+      formItem: 'switch',
+      text: 'E2 RES PEN',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+      disabled: e < 2,
+    },
+    e4Vuln: {
+      id: 'e4Vuln',
+      formItem: 'switch',
+      text: 'E4 Vulnerability',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+      disabled: e < 4,
+    },
+    e6BeBuff: {
+      id: 'e6BeBuff',
+      formItem: 'switch',
+      text: 'E6 BE buff',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+      disabled: e < 6,
+    },
   }
 
   const teammateContent: ContentDefinition<typeof teammateDefaults> = {
-    // foxianPrayer: content.foxianPrayer,
-    // be220Buff: {
-    //   id: 'be220Buff',
-    //   formItem: 'switch',
-    //   text: t('TeammateContent.be220Buff.text'),
-    //   content: t('TeammateContent.be220Buff.content'),
-    // },
-    // weaknessBreakBeStacks: {
-    //   id: 'weaknessBreakBeStacks',
-    //   formItem: 'slider',
-    //   text: t('TeammateContent.weaknessBreakBeStacks.text'),
-    //   content: t('TeammateContent.weaknessBreakBeStacks.content'),
-    //   min: 0,
-    //   max: 2,
-    // },
-    // defReduction: content.defReduction,
-    // superBreakDmg: content.superBreakDmg,
-    // e4BreakDmg: content.e4BreakDmg,
+    zoneActive: content.zoneActive,
+    ultDefPen: content.ultDefPen,
+    dancePartner: content.dancePartner,
+    beConversion: {
+      id: 'beConversion',
+      formItem: 'switch',
+      text: 'Break Effect conversion',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+    },
+    teammateBeValue: {
+      id: 'teammateBeValue',
+      formItem: 'slider',
+      text: `The Dahlia's Combat BE`,
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
+      min: 0,
+      max: 7.50,
+    },
+    spdBuff: content.spdBuff,
+    e1Buffs: content.e1Buffs,
+    e2ResPen: content.e2ResPen,
+    e4Vuln: content.e4Vuln,
   }
+
+  const fuaHits = e >= 4 ? 10 : 5
+
+  const hitMulti = ASHBLAZING_ATK_STACK
+    * (1 * 1)
 
   return {
     activeAbilities: [AbilityType.BASIC, AbilityType.ULT],
@@ -136,46 +188,70 @@ export default (e: Eidolon, withContent: boolean): CharacterConditionalsControll
     initializeConfigurations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r = action.characterConditionals as Conditionals<typeof content>
 
-      if (r.superBreakDmg) {
-        x.ENEMY_WEAKNESS_BROKEN.config(1, SOURCE_TALENT)
-      }
+      // if (r.superBreakDmg) {
+      //   x.ENEMY_WEAKNESS_BROKEN.config(1, SOURCE_TALENT)
+      // }
     },
     initializeTeammateConfigurations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r = action.characterConditionals as Conditionals<typeof content>
-      if (r.superBreakDmg) {
-        x.ENEMY_WEAKNESS_BROKEN.config(1, SOURCE_TALENT)
-      }
+      // if (r.superBreakDmg) {
+      //   x.ENEMY_WEAKNESS_BROKEN.config(1, SOURCE_TALENT)
+      // }
     },
     precomputeEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const r = action.characterConditionals as Conditionals<typeof content>
 
-      x.BE.buff(0.30, SOURCE_TRACE)
-
-      x.BREAK_EFFICIENCY_BOOST.buff((e >= 6 && r.e6BreakEfficiency) ? 0.50 : 0, SOURCE_E6)
-
       x.BASIC_ATK_SCALING.buff(basicScaling, SOURCE_BASIC)
+      x.SKILL_ATK_SCALING.buff(skillScaling, SOURCE_SKILL)
       x.ULT_ATK_SCALING.buff(ultScaling, SOURCE_ULT)
+      x.FUA_ATK_SCALING.buff(fuaScaling * fuaHits / context.enemyCount, SOURCE_TALENT)
+
+      // If broken
+      x.FUA_SUPER_BREAK_MODIFIER.buff(fuaSuperBreakScaling, SOURCE_TALENT)
+
+      x.BE.buff((e >= 6 && r.e6BeBuff) ? 1.50 : 0, SOURCE_E6)
+
+      //  Toughness Reduction taken by enemy targets while notWeakness Broken can be converted into Super Break DMG.
 
       x.BASIC_TOUGHNESS_DMG.buff(10, SOURCE_BASIC)
-      x.ULT_TOUGHNESS_DMG.buff(20, SOURCE_ULT)
+      x.SKILL_TOUGHNESS_DMG.buff(10, SOURCE_SKILL)
+      x.ULT_TOUGHNESS_DMG.buff(30, SOURCE_ULT)
+      x.FUA_TOUGHNESS_DMG.buff(3 * fuaHits / context.enemyCount, SOURCE_TALENT)
     },
     precomputeMutualEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const m = action.characterConditionals as Conditionals<typeof teammateContent>
 
-      x.BE.buffSingle((m.foxianPrayer) ? skillBeValue : 0, SOURCE_SKILL)
+      x.BREAK_EFFICIENCY_BOOST.buffTeam((m.zoneActive) ? 0.50 : 0, SOURCE_ULT)
+      x.DEF_PEN.buffTeam((m.ultDefPen) ? ultDefPenValue : 0, SOURCE_ULT)
 
-      x.SUPER_BREAK_MODIFIER.buffTeam((m.superBreakDmg) ? superBreakScaling : 0, SOURCE_TALENT)
-      x.DEF_PEN.buffTeam((m.defReduction) ? skillDefPenValue : 0, SOURCE_SKILL)
+      if (e >= 1 && m.e1Buffs) {
+        x.SUPER_BREAK_MODIFIER.buffTeam(superBreakScaling, SOURCE_TALENT)
+        x.SUPER_BREAK_MODIFIER.buff((m.dancePartner) ? 0.40 : 0, SOURCE_E1)
+      } else {
+        x.SUPER_BREAK_MODIFIER.buff((m.dancePartner) ? superBreakScaling : 0, SOURCE_TALENT)
+      }
 
-      x.BREAK_EFFICIENCY_BOOST.buffSingle((e >= 1 && m.foxianPrayer) ? 0.50 : 0, SOURCE_E1)
-      buffAbilityDmg(x, BREAK_DMG_TYPE, (e >= 4 && m.foxianPrayer && m.e4BreakDmg) ? 0.20 : 0, SOURCE_E4, Target.SINGLE)
+      // After "Dance Partner" uses an attack, deals an additional fixed amount of Toughness Reduction equal to 25% of the enemy target's Max Toughness (minimum of 10 points, up to 300 points).
+
+      x.SPD_P.buff((m.spdBuff) ? 0.30 : 0, SOURCE_TALENT)
+
+      x.RES_PEN.buffTeam((e >= 2 && m.e2ResPen) ? 0.18 : 0, SOURCE_E2)
+      x.VULNERABILITY.buffTeam((e >= 4 && m.e4Vuln) ? 0.12 * fuaHits : 0, SOURCE_E4)
     },
     precomputeTeammateEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const t = action.characterConditionals as Conditionals<typeof teammateContent>
 
-      x.BE.buffTeam(t.weaknessBreakBeStacks * (0.06 + (t.be220Buff ? 0.12 : 0)), SOURCE_TRACE)
+      const beBuff = (t.beConversion) ? t.teammateBeValue * 0.24 + 0.50 : 0
+      x.BE.buffTeam(beBuff, SOURCE_TALENT)
+      x.UNCONVERTIBLE_BE_BUFF.buffTeam(beBuff, SOURCE_TALENT)
     },
-    finalizeCalculations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {},
-    gpuFinalizeCalculations: (action: OptimizerAction, context: OptimizerContext) => '',
+    finalizeCalculations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
+      const r = action.characterConditionals as Conditionals<typeof content>
+
+      boostAshblazingAtkP(x, action, context, hitMulti)
+    },
+    gpuFinalizeCalculations: (action: OptimizerAction, context: OptimizerContext) => {
+      return gpuBoostAshblazingAtkP(hitMulti)
+    },
   }
 }
--- src/lib/conditionals/lightcone/5star/NeverForgetHerFlame.ts
+++ src/lib/conditionals/lightcone/5star/NeverForgetHerFlame.ts
@@ -1,14 +1,16 @@
+import i18next from 'i18next'
 import {
   Conditionals,
   ContentDefinition,
 } from 'lib/conditionals/conditionalUtils'
+import { CURRENT_DATA_VERSION } from 'lib/constants/constants'
 import { wgslTrue } from 'lib/gpu/injection/wgslUtils'
 import { Source } from 'lib/optimization/buffSource'
 import {
   ComputedStatsArray,
   Key,
 } from 'lib/optimization/computedStatsArray'
-import { NEVER_FORGER_HER_FLAME } from 'lib/simulations/tests/testMetadataConstants'
+import { NEVER_FORGET_HER_FLAME } from 'lib/simulations/tests/testMetadataConstants'
 import { TsUtils } from 'lib/utils/TsUtils'
 import { LightConeConditionalsController } from 'types/conditionals'
 import { SuperImpositionLevel } from 'types/lightCone'
@@ -18,46 +20,31 @@ import {
 } from 'types/optimizer'
 
 export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditionalsController => {
-  // const t = TsUtils.wrappedFixedT(withContent).get(null, 'conditionals', 'Lightcones.LiesAflutterInTheWind')
-  const { SOURCE_LC } = Source.lightCone(NEVER_FORGER_HER_FLAME)
+  // const t = TsUtils.wrappedFixedT(withContent).get(null, 'conditionals', 'Lightcones.NeverForgetHerFlame')
+  const { SOURCE_LC } = Source.lightCone(NEVER_FORGET_HER_FLAME)
 
-  const sValuesDefPen = [0.16, 0.18, 0.20, 0.22, 0.24]
-  const sValuesDefPenAdditional = [0.08, 0.09, 0.10, 0.11, 0.12]
+  const sValuesBreakDmg = [0.32, 0.42, 0.52, 0.62, 0.72]
 
   const defaults = {
-    defPen: true,
+    breakDmgBuff: true,
   }
 
   const teammateDefaults = {
-    defPen: true,
-    additionalDefPen: true,
+    breakDmgBuff: true,
   }
 
   const content: ContentDefinition<typeof defaults> = {
-    defPen: {
+    breakDmgBuff: {
       lc: true,
-      id: 'defPen',
+      id: 'breakDmgBuff',
       formItem: 'switch',
-      text: t('Content.defPen.text'),
-      content: t('Content.defPen.content', {
-        DefShred: TsUtils.precisionRound(100 * sValuesDefPen[s]),
-        AdditionalDefShred: TsUtils.precisionRound(100 * sValuesDefPenAdditional[s]),
-      }),
+      text: 'Break DMG buff',
+      content: i18next.t('BetaMessage', { ns: 'conditionals', Version: CURRENT_DATA_VERSION }),
     },
   }
 
   const teammateContent: ContentDefinition<typeof teammateDefaults> = {
-    defPen: content.defPen,
-    additionalDefPen: {
-      lc: true,
-      id: 'additionalDefPen',
-      formItem: 'switch',
-      text: t('TeammateContent.additionalDefPen.text'),
-      content: t('TeammateContent.additionalDefPen.content', {
-        DefShred: TsUtils.precisionRound(100 * sValuesDefPen[s]),
-        AdditionalDefShred: TsUtils.precisionRound(100 * sValuesDefPenAdditional[s]),
-      }),
-    },
+    breakDmgBuff: content.breakDmgBuff,
   }
 
   return {
@@ -70,26 +57,9 @@ export default (s: SuperImpositionLevel, withContent: boolean): LightConeConditi
     precomputeMutualEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
       const m = action.lightConeConditionals as Conditionals<typeof teammateContent>
 
-      x.DEF_PEN.buffTeam((m.defPen) ? sValuesDefPen[s] : 0, SOURCE_LC)
+      x.BREAK_DMG_BOOST.buffTeam((m.breakDmgBuff) ? sValuesBreakDmg[s] : 0, SOURCE_LC)
     },
-    precomputeTeammateEffects: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
-      const t = action.lightConeConditionals as Conditionals<typeof teammateContent>
-
-      x.DEF_PEN.buffTeam((t.defPen && t.additionalDefPen) ? sValuesDefPenAdditional[s] : 0, SOURCE_LC)
-    },
-    finalizeCalculations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {
-      const r = action.lightConeConditionals as Conditionals<typeof content>
-
-      x.DEF_PEN.buff((r.defPen && x.a[Key.SPD] >= 170) ? sValuesDefPenAdditional[s] : 0, SOURCE_LC)
-    },
-    gpuFinalizeCalculations: (action: OptimizerAction, context: OptimizerContext) => {
-      const r = action.lightConeConditionals as Conditionals<typeof content>
-
-      return `
-if (${wgslTrue(r.defPen)} && x.SPD >= 170) {
-  x.DEF_PEN += ${sValuesDefPenAdditional[s]};
-}
-      `
-    },
+    finalizeCalculations: (x: ComputedStatsArray, action: OptimizerAction, context: OptimizerContext) => {},
+    gpuFinalizeCalculations: (action: OptimizerAction, context: OptimizerContext) => '',
   }
 }

ee05f8e4be003f3d023df7844097983e68c04e12
Fribbels <fribbelsgithub@gmail.com>
2026-01-05 18:53:56 -0800

    feat: 4.0v1

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -2,10 +2,10 @@
 import gameData from 'data/game_data.json' with { type: 'json' }
 
 // Semver defined optimizer version
-export const CURRENT_OPTIMIZER_VERSION = 'v3.8.1'
+export const CURRENT_OPTIMIZER_VERSION = 'v4.0.1'
 
 // Represents the beta data content version, used for display but not for update notifications
-export const CURRENT_DATA_VERSION = '3.8v5'
+export const CURRENT_DATA_VERSION = '4.0v1'
 
 // Controls downtime messaging
 export const SHOWCASE_DOWNTIME = false

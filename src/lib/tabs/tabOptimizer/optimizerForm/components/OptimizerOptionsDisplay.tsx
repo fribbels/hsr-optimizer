dcd960159732567263594415d3564ac3ebe9b8c0
Fribbels <fribbelsgithub@gmail.com>
2026-03-07 03:56:52 -0800

    feat(store): convert OptimizerOptionsDisplay Form.Items to controlled (Phase 0d)

--- src/lib/tabs/tabOptimizer/optimizerForm/components/OptimizerOptionsDisplay.tsx
+++ src/lib/tabs/tabOptimizer/optimizerForm/components/OptimizerOptionsDisplay.tsx
@@ -4,7 +4,6 @@ import {
 } from '@ant-design/icons'
 import {
   Flex,
-  Form,
   Radio,
   Select,
   Switch,
@@ -15,7 +14,9 @@ import { Hint } from 'lib/interactions/hint'
 import { Assets } from 'lib/rendering/assets'
 import { generateCharacterList } from 'lib/rendering/displayUtils'
 import DB from 'lib/state/db'
+import { useOptimizerFormStore } from 'lib/stores/optimizerForm/useOptimizerFormStore'
 import { useCharacterTabStore } from 'lib/tabs/tabCharacters/useCharacterTabStore'
+import { recalculatePermutations } from 'lib/tabs/tabOptimizer/optimizerForm/optimizerFormActions'
 import {
   optimizerTabDefaultGap,
   panelWidth,
@@ -33,6 +34,16 @@ const OptimizerOptionsDisplay = (): JSX.Element => {
   const { t: tCharacters } = useTranslation('gameData', { keyPrefix: 'Characters' })
   const characters = useCharacterTabStore((s) => s.characters)
   const optimizerTabFocusCharacter = window.store((s) => s.optimizerTabFocusCharacter)
+
+  const includeEquippedRelics = useOptimizerFormStore((s) => s.includeEquippedRelics)
+  const rankFilter = useOptimizerFormStore((s) => s.rankFilter)
+  const keepCurrentRelics = useOptimizerFormStore((s) => s.keepCurrentRelics)
+  const rank = useOptimizerFormStore((s) => s.rank)
+  const exclude = useOptimizerFormStore((s) => s.exclude)
+  const enhance = useOptimizerFormStore((s) => s.enhance)
+  const grade = useOptimizerFormStore((s) => s.grade)
+  const mainStatUpscaleLevel = useOptimizerFormStore((s) => s.mainStatUpscaleLevel)
+  const deprioritizeBuffs = useOptimizerFormStore((s) => s.deprioritizeBuffs)
 
   const characterExcludeOptions = useMemo(() =>
     generateCharacterList(
@@ -76,38 +87,44 @@ const OptimizerOptionsDisplay = (): JSX.Element => {
         </Flex>
 
         <Flex align='center'>
-          <Form.Item name='includeEquippedRelics' valuePropName='checked'>
-            <Switch
-              checkedChildren={<CheckOutlined />}
-              unCheckedChildren={<CloseOutlined />}
-              defaultChecked
-              style={{ width: 45, marginRight: 5 }}
-            />
-          </Form.Item>
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            checked={includeEquippedRelics}
+            onChange={(val) => {
+              useOptimizerFormStore.getState().setRelicFilterField('includeEquippedRelics', val)
+              recalculatePermutations()
+            }}
+            style={{ width: 45, marginRight: 5 }}
+          />
           <Text>{t('AllowEquipped') /* Allow equipped relics */}</Text>
         </Flex>
 
         <Flex align='center'>
-          <Form.Item name='rankFilter' valuePropName='checked'>
-            <Switch
-              checkedChildren={<CheckOutlined />}
-              unCheckedChildren={<CloseOutlined />}
-              defaultChecked
-              style={{ width: 45, marginRight: 5 }}
-            />
-          </Form.Item>
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            checked={rankFilter}
+            onChange={(val) => {
+              useOptimizerFormStore.getState().setRelicFilterField('rankFilter', val)
+              recalculatePermutations()
+            }}
+            style={{ width: 45, marginRight: 5 }}
+          />
           <Text>{t('PriorityFilter') /* Character priority filter */}</Text>
         </Flex>
 
         <Flex align='center'>
-          <Form.Item name='keepCurrentRelics' valuePropName='checked'>
-            <Switch
-              checkedChildren={<CheckOutlined />}
-              unCheckedChildren={<CloseOutlined />}
-              defaultChecked
-              style={{ width: 45, marginRight: 5 }}
-            />
-          </Form.Item>
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            checked={keepCurrentRelics}
+            onChange={(val) => {
+              useOptimizerFormStore.getState().setRelicFilterField('keepCurrentRelics', val)
+              recalculatePermutations()
+            }}
+            style={{ width: 45, marginRight: 5 }}
+          />
           <Text>{t('KeepCurrent') /* Keep current relics */}</Text>
         </Flex>
 
@@ -116,38 +133,48 @@ const OptimizerOptionsDisplay = (): JSX.Element => {
             <HeaderText>
               {t('Priority.Header') /* Priority */}
             </HeaderText>
-            <Form.Item name='rank'>
-              <Select
-                style={{ width: (panelWidth - optimizerTabDefaultGap) / 2 }}
-                options={characterPriorityOptions}
-                popupMatchSelectWidth={225}
-                listHeight={500}
-                optionLabelProp='name'
-                placeholder={t('Priority.Header') /* Priority */}
-                showSearch
-                filterOption={Utils.nameFilterOption}
-              />
-            </Form.Item>
+            <Select
+              style={{ width: (panelWidth - optimizerTabDefaultGap) / 2 }}
+              options={characterPriorityOptions}
+              value={rank}
+              onChange={(val) => {
+                useOptimizerFormStore.getState().setRelicFilterField('rank', val)
+                const characterId = useOptimizerFormStore.getState().characterId
+                if (characterId && DB.getCharacterById(characterId)) {
+                  DB.insertCharacter(characterId, val)
+                }
+                recalculatePermutations()
+              }}
+              popupMatchSelectWidth={225}
+              listHeight={500}
+              optionLabelProp='name'
+              placeholder={t('Priority.Header') /* Priority */}
+              showSearch
+              filterOption={Utils.nameFilterOption}
+            />
           </Flex>
           <Flex vertical gap={2}>
             <HeaderText>
               {t('Exclude') /* Exclude */}
             </HeaderText>
-            <Form.Item name='exclude'>
-              <Select
-                style={{ width: (panelWidth - optimizerTabDefaultGap) / 2, height: 32 }}
-                mode='multiple'
-                maxTagCount='responsive'
-                popupMatchSelectWidth={250}
-                listHeight={500}
-                allowClear
-                showSearch
-                optionLabelProp='title'
-                placeholder={t('Exclude') /* Exclude */}
-                options={characterExcludeOptions}
-                filterOption={Utils.titleFilterOption}
-              />
-            </Form.Item>
+            <Select
+              style={{ width: (panelWidth - optimizerTabDefaultGap) / 2, height: 32 }}
+              mode='multiple'
+              maxTagCount='responsive'
+              popupMatchSelectWidth={250}
+              listHeight={500}
+              allowClear
+              showSearch
+              optionLabelProp='title'
+              placeholder={t('Exclude') /* Exclude */}
+              options={characterExcludeOptions}
+              value={exclude}
+              onChange={(val) => {
+                useOptimizerFormStore.getState().setRelicFilterField('exclude', val)
+                recalculatePermutations()
+              }}
+              filterOption={Utils.titleFilterOption}
+            />
           </Flex>
         </Flex>
 
@@ -156,36 +183,42 @@ const OptimizerOptionsDisplay = (): JSX.Element => {
             <HeaderText>
               {t('MinEnhance.Header') /* Min enhance */}
             </HeaderText>
-            <Form.Item name='enhance'>
-              <Select
-                style={{ width: (panelWidth - optimizerTabDefaultGap) / 2 }}
-                options={[
-                  { value: 0, label: t('MinEnhance.Label0') }, // '+0'
-                  { value: 3, label: t('MinEnhance.Label3') }, // '+3'
-                  { value: 6, label: t('MinEnhance.Label6') }, // '+6'
-                  { value: 9, label: t('MinEnhance.Label9') }, // '+9'
-                  { value: 12, label: t('MinEnhance.Label12') }, // '+12'
-                  { value: 15, label: t('MinEnhance.Label15') }, // '+15'
-                ]}
-              />
-            </Form.Item>
+            <Select
+              style={{ width: (panelWidth - optimizerTabDefaultGap) / 2 }}
+              value={enhance}
+              onChange={(val) => {
+                useOptimizerFormStore.getState().setRelicFilterField('enhance', val)
+                recalculatePermutations()
+              }}
+              options={[
+                { value: 0, label: t('MinEnhance.Label0') }, // '+0'
+                { value: 3, label: t('MinEnhance.Label3') }, // '+3'
+                { value: 6, label: t('MinEnhance.Label6') }, // '+6'
+                { value: 9, label: t('MinEnhance.Label9') }, // '+9'
+                { value: 12, label: t('MinEnhance.Label12') }, // '+12'
+                { value: 15, label: t('MinEnhance.Label15') }, // '+15'
+              ]}
+            />
           </Flex>
 
           <Flex vertical gap={2}>
             <HeaderText>
               {t('MinRarity.Header') /* Min rarity */}
             </HeaderText>
-            <Form.Item name='grade'>
-              <Select
-                style={{ width: (panelWidth - optimizerTabDefaultGap) / 2 }}
-                options={[
-                  { value: 2, label: t('MinRarity.Label2') }, // '2 ★ +'
-                  { value: 3, label: t('MinRarity.Label3') }, // '3 ★ +'
-                  { value: 4, label: t('MinRarity.Label4') }, // '4 ★ +'
-                  { value: 5, label: t('MinRarity.Label5') }, // '5 ★'
-                ]}
-              />
-            </Form.Item>
+            <Select
+              style={{ width: (panelWidth - optimizerTabDefaultGap) / 2 }}
+              value={grade}
+              onChange={(val) => {
+                useOptimizerFormStore.getState().setRelicFilterField('grade', val)
+                recalculatePermutations()
+              }}
+              options={[
+                { value: 2, label: t('MinRarity.Label2') }, // '2 ★ +'
+                { value: 3, label: t('MinRarity.Label3') }, // '3 ★ +'
+                { value: 4, label: t('MinRarity.Label4') }, // '4 ★ +'
+                { value: 5, label: t('MinRarity.Label5') }, // '5 ★'
+              ]}
+            />
           </Flex>
         </Flex>
 
@@ -194,19 +227,22 @@ const OptimizerOptionsDisplay = (): JSX.Element => {
             <HeaderText>
               {t('BoostMain.Header') /* Boost main stat */}
             </HeaderText>
-            <Form.Item name='mainStatUpscaleLevel'>
-              <Select
-                style={{ width: (panelWidth - optimizerTabDefaultGap) / 2 }}
-                options={[
-                  { value: 0, label: t('BoostMain.Label0') }, // '+0'
-                  { value: 3, label: t('BoostMain.Label3') }, // '+3'
-                  { value: 6, label: t('BoostMain.Label6') }, // '+6'
-                  { value: 9, label: t('BoostMain.Label9') }, // '+9'
-                  { value: 12, label: t('BoostMain.Label12') }, // '+12'
-                  { value: 15, label: t('BoostMain.Label15') }, // '+15'
-                ]}
-              />
-            </Form.Item>
+            <Select
+              style={{ width: (panelWidth - optimizerTabDefaultGap) / 2 }}
+              value={mainStatUpscaleLevel}
+              onChange={(val) => {
+                useOptimizerFormStore.getState().setRelicFilterField('mainStatUpscaleLevel', val)
+                recalculatePermutations()
+              }}
+              options={[
+                { value: 0, label: t('BoostMain.Label0') }, // '+0'
+                { value: 3, label: t('BoostMain.Label3') }, // '+3'
+                { value: 6, label: t('BoostMain.Label6') }, // '+6'
+                { value: 9, label: t('BoostMain.Label9') }, // '+9'
+                { value: 12, label: t('BoostMain.Label12') }, // '+12'
+                { value: 15, label: t('BoostMain.Label15') }, // '+15'
+              ]}
+            />
           </Flex>
         </Flex>
 
@@ -215,28 +251,28 @@ const OptimizerOptionsDisplay = (): JSX.Element => {
             <HeaderText>
               {t('DPSMode.Header') /* DPS Mode */}
             </HeaderText>
-            <Form.Item name='deprioritizeBuffs'>
-              <Radio.Group
-                size='small'
-                optionType='button'
-                buttonStyle='solid'
-                style={{ width: '100%', display: 'flex' }}
+            <Radio.Group
+              size='small'
+              optionType='button'
+              buttonStyle='solid'
+              style={{ width: '100%', display: 'flex' }}
+              value={deprioritizeBuffs}
+              onChange={(e) => useOptimizerFormStore.getState().setDeprioritizeBuffs(e.target.value)}
+            >
+              <Radio
+                style={{ display: 'flex', flex: 1, justifyContent: 'center', paddingInline: 0 }}
+                value={false}
               >
-                <Radio
-                  style={{ display: 'flex', flex: 1, justifyContent: 'center', paddingInline: 0 }}
-                  value={false}
-                >
-                  {t('DPSMode.Main') /* Main */}
-                </Radio>
-                <Radio
-                  style={{ display: 'flex', flex: 1, justifyContent: 'center', paddingInline: 0 }}
-                  value={true}
-                  defaultChecked
-                >
-                  {t('DPSMode.Sub') /* Sub */}
-                </Radio>
-              </Radio.Group>
-            </Form.Item>
+                {t('DPSMode.Main') /* Main */}
+              </Radio>
+              <Radio
+                style={{ display: 'flex', flex: 1, justifyContent: 'center', paddingInline: 0 }}
+                value={true}
+                defaultChecked
+              >
+                {t('DPSMode.Sub') /* Sub */}
+              </Radio>
+            </Radio.Group>
           </Flex>
         </Flex>
       </Flex>

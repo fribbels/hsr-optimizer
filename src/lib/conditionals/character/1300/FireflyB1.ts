e62b61de664cacb148c30714525dbf10490a1f00
Fribbels <fribbelsgithub@gmail.com>
2026-03-16 22:54:17 -0700

    feat: firefly

--- src/lib/conditionals/character/1300/FireflyB1.ts
+++ src/lib/conditionals/character/1300/FireflyB1.ts
@@ -49,7 +49,7 @@ import {
   WHOLE_SKILL,
 } from 'lib/optimization/rotation/turnAbilityConfig'
 import { SortOption } from 'lib/optimization/sortOptions'
-import { SPREAD_RELICS_4P_GENERAL_CONDITIONALS, } from 'lib/scoring/scoringConstants'
+import { SPREAD_RELICS_4P_GENERAL_CONDITIONALS } from 'lib/scoring/scoringConstants'
 import { TsUtils } from 'lib/utils/TsUtils'
 
 import { Eidolon } from 'types/character'
@@ -73,7 +73,7 @@ export const FireflyB1Abilities: AbilityKind[] = [
 ]
 
 const conditionals = (e: Eidolon, withContent: boolean): CharacterConditionalsController => {
-  const t = TsUtils.wrappedFixedT(withContent).get(null, 'conditionals', 'Characters.FireflyB1')
+  const t = TsUtils.wrappedFixedT(withContent).get(null, 'conditionals', 'Characters.Firefly')
   const { basic, skill, ult, talent } = AbilityEidolon.SKILL_BASIC_3_ULT_TALENT_5
   const {
     SOURCE_BASIC,

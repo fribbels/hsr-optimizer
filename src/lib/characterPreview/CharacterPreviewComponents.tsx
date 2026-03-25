e14e06e69e824d02c75429dd2e4a3561f0a48d51
Fribbels <fribbelsgithub@gmail.com>
2026-03-24 21:40:46 -0700

    feat: cleanup

--- src/lib/characterPreview/CharacterPreviewComponents.tsx
+++ src/lib/characterPreview/CharacterPreviewComponents.tsx
@@ -21,8 +21,12 @@ function isMobileOrSafari() {
 }
 
 // Mobile/Safari shadows don't render correctly on the Y axis
-export const showcaseShadow = isMobileOrSafari() ? 'rgb(0, 0, 0) 1px 0px 6px' : 'rgb(0, 0, 0) 1px 1px 6px'
-export const showcaseShadowInsetAddition = ', inset rgb(255 255 255 / 30%) 0px 0px 2px'
+const showcaseShadowDefault = isMobileOrSafari() ? 'rgb(0, 0, 0) 1px 0px 6px' : 'rgb(0, 0, 0) 1px 1px 6px'
+const showcaseShadowInsetDefault = ', inset rgb(255 255 255 / 30%) 0px 0px 2px'
+
+// Use CSS custom properties so the debug slider panel can override these
+export const showcaseShadow = `var(--showcase-shadow, ${showcaseShadowDefault})`
+export const showcaseShadowInsetAddition = `var(--showcase-shadow-inset, ${showcaseShadowInsetDefault})`
 export const showcaseTransition = 'background-color 0.35s, box-shadow 0.25s, border-color 0.25s'
 export const showcaseOutlineLight = 'rgba(255, 255, 255, 0.20) solid 1px'
 export const showcaseButtonStyle: CSSProperties = {

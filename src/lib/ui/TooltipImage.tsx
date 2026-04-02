7a118c7857a05e35c9f6313d7ac149633a25e21b
Fribbels <fribbelsgithub@gmail.com>
2026-04-02 16:33:12 -0700

    fix: tooltip hovers

--- src/lib/ui/TooltipImage.tsx
+++ src/lib/ui/TooltipImage.tsx
@@ -1,15 +1,15 @@
-import { Popover, Text } from '@mantine/core'
+import { HoverCard, Text } from '@mantine/core'
 import type { HintContent } from 'lib/interactions/hint'
 import { Assets } from 'lib/rendering/assets'
 
 export const TooltipImage = (props: { type: HintContent }) => (
-  <Popover width={500}>
-    <Popover.Target>
+  <HoverCard width={500} openDelay={200} closeDelay={100}>
+    <HoverCard.Target>
       <img src={Assets.getQuestion()} style={{ width: 16, opacity: 0.6, cursor: 'pointer' }} />
-    </Popover.Target>
-    <Popover.Dropdown>
+    </HoverCard.Target>
+    <HoverCard.Dropdown>
       <Text fw={600} mb={4}>{props.type.title}</Text>
       {props.type.content}
-    </Popover.Dropdown>
-  </Popover>
+    </HoverCard.Dropdown>
+  </HoverCard>
 )

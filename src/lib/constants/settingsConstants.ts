ad888345a6d0e8417ea74fe83d482812616bc464
Fribbels <fribbelsgithub@gmail.com>
2026-04-14 23:15:15 -0700

    fix: settings consts

---
+++ src/lib/constants/settingsConstants.ts
@@ -0,0 +1,33 @@
+import { type UserSettings } from 'types/store'
+
+export const SettingOptions = {
+  RelicEquippingBehavior: {
+    Replace: 'Replace',
+    Swap: 'Swap',
+  },
+  PermutationsSidebarBehavior: {
+    ShowXL: 'Show XL',
+    ShowXXL: 'Show XXL',
+    NoShow: 'Do Not Show',
+  },
+  ExpandedInfoPanelPosition: {
+    Above: 'Above',
+    Below: 'Below',
+  },
+  ShowLocatorInRelicsModal: {
+    No: 'No',
+    Yes: 'Yes',
+  },
+  ShowComboDmgWarning: {
+    Show: 'Show',
+    Hide: 'Hide',
+  },
+} as const satisfies Record<keyof UserSettings, Record<string, string>>
+
+export const DefaultSettingOptions: Record<keyof UserSettings, string> = {
+  RelicEquippingBehavior: SettingOptions.RelicEquippingBehavior.Replace,
+  PermutationsSidebarBehavior: SettingOptions.PermutationsSidebarBehavior.ShowXL,
+  ExpandedInfoPanelPosition: SettingOptions.ExpandedInfoPanelPosition.Below,
+  ShowLocatorInRelicsModal: SettingOptions.ShowLocatorInRelicsModal.No,
+  ShowComboDmgWarning: SettingOptions.ShowComboDmgWarning.Show,
+}

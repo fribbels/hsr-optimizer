4ebec380c628e6693e822f8eef146158cdd0c42c
Floods <lucasrobertson368+github@gmail.com>
2026-05-03 01:17:40 +0100

    defualt blank sim request to empty values

--- src/lib/simulations/utils/requestUtils.ts
+++ src/lib/simulations/utils/requestUtils.ts
@@ -1,18 +1,18 @@
-import {
-  Sets,
-  Stats,
-} from 'lib/constants/constants'
+import type {
+  SetsOrnaments,
+  SetsRelics,
+} from 'lib/sets/setConfigRegistry'
 import type { SimulationRequest } from 'lib/simulations/statSimulationTypes'
 
 export function blankSimRequest(): SimulationRequest {
   return {
-    simBody: Stats.CR,
-    simFeet: Stats.ATK_P,
-    simPlanarSphere: Stats.Wind_DMG,
-    simLinkRope: Stats.ERR,
-    simOrnamentSet: Sets.SprightlyVonwacq,
-    simRelicSet1: Sets.EagleOfTwilightLine,
-    simRelicSet2: Sets.EagleOfTwilightLine,
+    simBody: '',
+    simFeet: '',
+    simPlanarSphere: '',
+    simLinkRope: '',
+    simRelicSet1: '' as SetsRelics,
+    simRelicSet2: '' as SetsRelics,
+    simOrnamentSet: '' as SetsOrnaments,
     stats: {},
   }
 }

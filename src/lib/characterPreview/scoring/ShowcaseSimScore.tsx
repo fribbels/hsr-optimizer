a035cec9763d18f842c5a20f491a0b7a468ee2bf
Fribbels <fribbelsgithub@gmail.com>
2026-05-17 01:48:06 -0700

    fix: beta 2

--- src/lib/characterPreview/scoring/ShowcaseSimScore.tsx
+++ src/lib/characterPreview/scoring/ShowcaseSimScore.tsx
@@ -248,7 +248,7 @@ export const ShowcaseScoreHeader = memo(function ShowcaseScoreHeader({ relics, t
     <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }} className={styles.scoreHeaderWrapper}>
       {!isDps && (
         <StatText className={styles.scoreHeaderText}>
-          [ BETA 1 ]
+          [ BETA 2 ]
         </StatText>
       )}
       <ShowcaseScoreHeaderReady relics={relics} configType={configType} t={t} />

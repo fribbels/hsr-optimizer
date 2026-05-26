86feec275de4f15beb0fada3f77c3e22cada7bea
Fribbels <fribbelsgithub@gmail.com>
2026-05-25 21:47:06 -0700

    feat: remove fuxuan, clean up label

--- src/lib/conditionals/character/1200/FuXuan.ts
+++ src/lib/conditionals/character/1200/FuXuan.ts
@@ -1,6 +1,3 @@
-import { Sunday } from 'lib/conditionals/character/1300/Sunday'
-import { Cerydra } from 'lib/conditionals/character/1400/Cerydra'
-import { Phainon } from 'lib/conditionals/character/1400/Phainon'
 import {
   AbilityEidolon,
   type Conditionals,
@@ -12,9 +9,6 @@ import {
   gpuDynamicStatConversion,
 } from 'lib/conditionals/evaluation/statConversion'
 import { HitDefinitionBuilder } from 'lib/conditionals/hitDefinitionBuilder'
-import { AGroundedAscent } from 'lib/conditionals/lightcone/5star/AGroundedAscent'
-import { EpochEtchedInGoldenBlood } from 'lib/conditionals/lightcone/5star/EpochEtchedInGoldenBlood'
-import { ThusBurnsTheDawn } from 'lib/conditionals/lightcone/5star/ThusBurnsTheDawn'
 import {
   ConditionalActivation,
   ConditionalType,
@@ -32,17 +26,12 @@ import {
 import { type ComputedStatsContainer } from 'lib/optimization/engine/container/computedStatsContainer'
 import { SortOption } from 'lib/optimization/sortOptions'
 import { PresetEffects } from 'lib/scoring/presetEffects'
-import {
-  SPREAD_ORNAMENTS_2P_SUPPORT,
-  SPREAD_RELICS_4P_SHIELD,
-} from 'lib/scoring/scoringConstants'
 import { wrappedFixedT } from 'lib/utils/i18nUtils'
 
 import { type Eidolon } from 'types/character'
 import { type CharacterConfig } from 'types/characterConfig'
 import {
   type ScoringMetadata,
-  type SimulationMetadata,
 } from 'types/metadata'
 
 import { type CharacterConditionalsController } from 'types/conditionals'
@@ -53,8 +42,6 @@ import {
 
 import {
   AbilityKind,
-  DEFAULT_TALENT_HEAL,
-  NULL_TURN_ABILITY_NAME,
 } from 'lib/optimization/rotation/turnAbilityConfig'
 import { precisionRound } from 'lib/utils/mathUtils'
 export const FuXuanEntities = createEnum('FuXuan')
@@ -276,58 +263,6 @@ const conditionals = (e: Eidolon, withContent: boolean): CharacterConditionalsCo
   }
 }
 
-const healSimulation = (): SimulationMetadata => ({
-  parts: {
-    [Parts.Body]: [Stats.HP_P],
-    [Parts.Feet]: [Stats.HP_P, Stats.SPD],
-    [Parts.PlanarSphere]: [Stats.HP_P],
-    [Parts.LinkRope]: [Stats.HP_P, Stats.ERR],
-  },
-  substats: [
-    Stats.HP_P,
-    Stats.DEF_P,
-    Stats.HP,
-    Stats.DEF,
-    Stats.SPD,
-    Stats.RES,
-  ],
-  errRopeEidolon: 0,
-  comboTurnAbilities: [
-    NULL_TURN_ABILITY_NAME,
-    DEFAULT_TALENT_HEAL,
-    DEFAULT_TALENT_HEAL,
-  ],
-  relicSets: [
-    [Sets.WarriorGoddessOfSunAndThunder, Sets.WarriorGoddessOfSunAndThunder],
-    ...SPREAD_RELICS_4P_SHIELD,
-  ],
-  ornamentSets: [
-    Sets.LushakaTheSunkenSeas,
-    ...SPREAD_ORNAMENTS_2P_SUPPORT,
-  ],
-  teammates: [
-    {
-      characterId: Phainon.id,
-      lightCone: ThusBurnsTheDawn.id,
-      characterEidolon: 0,
-      lightConeSuperimposition: 1,
-    },
-    {
-      characterId: Cerydra.id,
-      lightCone: EpochEtchedInGoldenBlood.id,
-      characterEidolon: 0,
-      lightConeSuperimposition: 1,
-    },
-    {
-      characterId: Sunday.id,
-      lightCone: AGroundedAscent.id,
-      characterEidolon: 0,
-      lightConeSuperimposition: 1,
-    },
-  ],
-  deprioritizeBuffs: true,
-})
-
 const scoring = (): ScoringMetadata => ({
   stats: {
     [Stats.ATK]: 0,
@@ -373,7 +308,6 @@ const scoring = (): ScoringMetadata => ({
     SortOption.FUA,
     SortOption.DOT,
   ],
-  healSimulation: healSimulation(),
 })
 
 const display = {

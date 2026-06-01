da5707e7f9307d5e0cde46125c134dae21cc264d
Fribbels <fribbelsgithub@gmail.com>
2026-05-31 22:31:54 -0700

    chore: Change RELIQUARY_DOWNTIME to false

--- src/lib/constants/constants.ts
+++ src/lib/constants/constants.ts
@@ -11,7 +11,7 @@ export const CURRENT_DATA_VERSION = '4.3v5'
 
 // Controls downtime messaging
 export const SHOWCASE_DOWNTIME = true
-export const RELIQUARY_DOWNTIME = true
+export const RELIQUARY_DOWNTIME = false
 export const DOWNTIME_VERSION: string = '4.3'
 
 export const CharacterAnnouncementMessages: Record<string, string> = {

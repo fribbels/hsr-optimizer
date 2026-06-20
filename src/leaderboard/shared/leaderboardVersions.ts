eeede94234f04cfd0a718cf3d7d41ab716efb3e2
Fribbels <fribbelsgithub@gmail.com>
2026-06-19 22:35:40 -0700

    feat: eva rescore

--- src/leaderboard/shared/leaderboardVersions.ts
+++ src/leaderboard/shared/leaderboardVersions.ts
@@ -2,6 +2,6 @@ import type { LeaderboardVersionFile } from 'leaderboard/shared/types'
 
 export const LEADERBOARD_VERSIONS: LeaderboardVersionFile = {
   global: 1,
-  characters: { '1015': 1, '1402': 2 },
+  characters: { '1015': 1, '1402': 2, '1500': 1 },
   lightCones: {},
 }
